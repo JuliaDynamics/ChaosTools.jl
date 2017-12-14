7e60fd15050833bc00c0f41b15aeecad389392b2
Datseris <datseris.george@gmail.com>
2017-12-14 22:35:11 +0100

    Initial Source copy

---
+++ src/ChaosTools.jl
@@ -0,0 +1,31 @@
+__precompile__()
+
+"""
+Tools for the exploration of chaos and nonlinear dynamics 
+"""
+module ChaosTools
+
+using Reexport
+@reexport using DynamicalSystemsDef
+
+# Lyapunovs:
+include("lyapunovs.jl")
+
+# Entropies and Dimension Estimation:
+include("dimensions/entropies.jl")
+include("dimensions/dims.jl")
+
+# Nonlinear Timeseries Analysis:
+include("delay_coords.jl")
+
+# Periodicity:
+include("periodic.jl")
+
+# Chaos Detection:
+include("chaos_detection.jl")
+
+# Visualization routines:
+using Requires
+@require PyPlot include("visualizations.jl")
+
+end # module

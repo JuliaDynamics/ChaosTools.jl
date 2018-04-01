a20ebe331258f9bb5a8d263afd8c64e7055b3783
Datseris <datseris.george@gmail.com>
2018-04-01 14:52:50 +0200

    fix forgoten renyi in tests

--- test/entropy_dimension.jl
+++ test/entropy_dimension.jl
@@ -5,19 +5,19 @@ using Base.Test
 
 test_value = (val, vmin, vmax) -> @test vmin <= val <= vmax
 
-println("\nTesting generalized entropy (renyi) & linear scaling...")
+println("\nTesting generalized entropy (genentropy) & linear scaling...")
 @testset "Generalized Dimensions" begin
   @testset "Henon Map" begin
     ds = Systems.henon()
     ts = trajectory(ds, 200000)
     mat = convert(Matrix, ts)
     # Test call with dataset
-    renyi(1, 0.001, ts)
+    genentropy(1, 0.001, ts)
     es = logspace(-0, -3, 7)
     dd = zeros(es)
     for q in [0,2,1, 2.56]
       for (i, ee) in enumerate(es)
-        dd[i] = renyi(q, ee, mat)
+        dd[i] = genentropy(q, ee, mat)
       end
       linr, dim = linear_region(-log.(es), dd)
       test_value(dim, 1.1, 1.3)
@@ -30,7 +30,7 @@ println("\nTesting generalized entropy (renyi) & linear scaling...")
     dd = zeros(es)
     for q in [0,1,2]
       for (i, ee) in enumerate(es)
-        dd[i] = renyi(q, ee, ts)
+        dd[i] = genentropy(q, ee, ts)
       end
       linr, dim = linear_region(-log.(es), dd)
       if q == 0

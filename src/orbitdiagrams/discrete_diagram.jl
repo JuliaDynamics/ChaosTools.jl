8ac7c7eeac835abf4c68f9f07f0a554df4697298
George Datseris <datseris.george@gmail.com>
2018-07-31 13:19:18 +0200

    rework discrete diagram

--- src/orbitdiagrams/discrete_diagram.jl
+++ src/orbitdiagrams/discrete_diagram.jl
@@ -2,48 +2,77 @@ export orbitdiagram
 
 """
     orbitdiagram(ds::DiscreteDynamicalSystem, i, p_index, pvalues; kwargs...)
-Compute the orbit diagram (also called bifurcation diagram) of the given system
-for the `i`-th variable for parameter values `pvalues`. The `p_index` specifies
+Compute the orbit diagram (also called bifurcation diagram) of the given system,
+saving the `i` variable(s) for parameter values `pvalues`. The `p_index` specifies
 which parameter of the equations of motion is to be changed.
 
-Returns a vector of vectors, where each entry are the points are each parameter value.
+`i` can be `Int` or `Vector{Int}`.
+If `i` is `Int`, returns a vector of vectors. Else
+it returns vectors of vectors of vectors.
+Each entry are the points at each parameter value.
 
 ## Keyword Arguments
-* `ics = [get_state(ds)]` : container of initial conditions that
-  are used at each parameter value to evolve orbits.
 * `Ttr::Int = 1000` : Transient steps;
   each orbit is evolved for `Ttr` first before saving output.
 * `n::Int = 100` : Amount of points to save for each initial condition.
+* `dt = 1` : Stepping time. Changing this will give you the orbit diagram of
+  the `dt` order map.
+* `u0 = get_state(ds)` : Initial condition. Besides a vector you can also give
+  a vector of vectors such as `length(u0) == length(pvalues)`. Then each parameter
+  has a different initial condition.
 
 See also [`poincaresos`](@ref) and [`produce_orbitdiagram`](@ref).
 """
-function orbitdiagram(ds::DDS{IIP, S, D}, i::Int, p_index, pvalues;
-    n::Int = 100, Ttr::Int = 1000, ics = [get_state(ds)]) where {IIP, S, D}
+function orbitdiagram(ds::DDS{IIP, S, D}, idxs, p_index, pvalues;
+    n::Int = 100, Ttr::Int = 1000, u0 = get_state(ds), dt = 1) where {IIP, S, D}
 
     p0 = ds.p[p_index]
     if D == 1
-        i != 1 &&
+        idxs != 1 &&
         error("You have a 1D system and yet you gave `i=$i`. What's up with that!?")
     end
 
-    output = [zeros(eltype(S), n*length(ics)) for i in 1:length(pvalues)]
+    typeof(u0) <: Vector{<:AbstractVector} && @assert length(u0)==length(p)
 
+    i = typeof(idxs) <: Int ? i : SVector{length(idxs), Int}(idxs...)
+
+    output = _initialize_output(ds.u0, i, n, length(pvalues))
     integ = integrator(ds)
+    _fill_orbitdiagram!(output, integ, i, pvalues, p_index, n, Ttr, u0, dt)
+    ds.p[p_index] = p0
+    return output
+end
+
+
+function _initialize_output(u::S, i::Int, n, l) where {S}
+    output = [zeros(eltype(S), n) for k in 1:l]
+end
+function _initialize_output(u::S, i::SVector, n, l) where {S}
+    s = u[i]
+    output = [Vector{typeof(s)}(undef, n) for k in 1:l]
+end
+
+
+function _fill_orbitdiagram!(output, integ, i, pvalues, p_index,
+    n, Ttr, u0, dt)
 
     for (j, p) in enumerate(pvalues)
 
         integ.p[p_index] = p
 
-        for (m, st) in enumerate(ics)
-            reinit!(integ, st)
-            step!(integ, Ttr)
+        if typeof(u0) <: Vector{<:AbstractVector}
+            st = u0[j]
+        else
+            st = u0
+        end
 
-            for k in 1:n
-                step!(integ)
-                output[j][(m-1)*n + k] = integ.u[i]
-            end
+        reinit!(integ, st)
+        step!(integ, Ttr)
+
+        for k in 1:n
+            step!(integ, dt)
+            @inbounds output[j][k] = integ.u[i]
         end
     end
-    ds.p[p_index] = p0
     return output
 end

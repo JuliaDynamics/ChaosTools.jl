508162e3472a4d50a821b7530dee1d7e943b3791
George Datseris <datseris.george@gmail.com>
2018-11-09 13:17:05 +0100

    small reorg

--- src/ChaosTools.jl
+++ src/ChaosTools.jl
@@ -12,24 +12,18 @@ using DynamicalSystemsBase: stateeltype
 using OrdinaryDiffEq
 import OrdinaryDiffEq: ODEIntegrator
 
-# Lyapunovs:
 include("lyapunovs.jl")
 
-# Phase space related:
 include("orbitdiagrams/discrete_diagram.jl")
 include("orbitdiagrams/poincare.jl")
 
-# Entropies and Dimension Estimation:
 include("dimensions/entropies.jl")
 include("dimensions/dims.jl")
 
-# Nonlinear Timeseries Analysis:
 include("nlts.jl")
 
-# Periodicity:
 include("periodic.jl")
 
-# Chaos Detection:
 include("chaos_detection.jl")
 
 # Ugly methods that shouldn't exist:

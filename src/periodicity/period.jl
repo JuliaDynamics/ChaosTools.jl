5d5f1fdb37afeb0da631f6f38d6d6ce158a8fbbc
George Datseris <datseris.george@gmail.com>
2019-06-24 20:59:42 +0200

    re-work method to first find all local maxima!

--- src/periodicity/period.jl
+++ src/periodicity/period.jl
@@ -7,8 +7,10 @@ Estimate the period of the signal `v`, with accompanying time vector `t`,
 using the given `method`:
 
 * `"ac"` : Use the autocorrelation function (AC). The value where the AC first
-  comes back close to 1 is the period of the signal. The keyword `ε = 0.05`
-  here identifies how close to 1 actually counts as 1.
+  comes back close to 1 is the period of the signal. The keyword
+  `L = length(v)÷10` denotes the length of the AC (thus, given the default
+  setting, this method will fail if there less than 10 periods in the signal).
+  The keyword `ε = 0.2` means that `1-ε` counts as "1" for the AC.
 """
 function estimate_period(v, t, method; kwargs...)
     @assert length(v) == length(t)
@@ -16,28 +18,23 @@ function estimate_period(v, t, method; kwargs...)
         error("Unknown method given to `estimate_period`.")
     elseif method == "ac"
         period = _ac_period(v, t; kwargs...)
-
+    end
     return period
 end
 
-function _ac_period(v, t; ε = 0.05)
+function _ac_period(v, t; ε = 0.2, L = length(v)÷10)
     err = "The autocorrelation did not become close to 1."
-    ac = autocor(v)
+    ac = autocor(v, 0:L)
     j = 1
-    # find next local maximum of ac
-    while j < length(ac) && ac[j] ≥ ac[j+1]
-        j += 1
+    local_maxima = findall(i -> ac[i-1] < ac[i] ≥ ac[i+1], 2:length(ac)-1)
+    # progressively scan the local maxima and find the first within 1-ε
+    for i in local_maxima
+        if ac[i] > 1-ε
+            j = i
+            break
+        end
     end
-    # now j is a local minimum
-    j == length(ac) && error(err)
-    while j < length(ac) && ac[j] < ac[j+1]
-        j += 1
-    end
-    # now j is a local maximum
-    if j == length(ac) || ac[j] < 1 - ε
-        error(err)
-    end
-    # since now it holds that ac[j] is next local maximum within 1-ε:
+    # since now it holds that ac[j] is a local maximum within 1-ε:
     period = t[j] - t[1]
     return period
 end

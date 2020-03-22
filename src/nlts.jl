9e08b938191c82127fe65db384e6fdef073fffbe
Anshul Singhvi <asinghvi17@simons-rock.edu>
2020-03-22 07:50:07 +0530

    Add footnotes to docstring

--- src/nlts.jl
+++ src/nlts.jl
@@ -24,7 +24,7 @@ that are evolved in time for `k` steps (`k` must be integer).
   that the algorithm is applied for all state indices contained in `refstates`.
 * `w::Int = 1` : The Theiler window, which determines
   whether points are separated enough in time to be considered separate trajectories
-  (see [1] and [`neighborhood`](@ref)).
+  (see [^1] and [`neighborhood`](@ref)).
 * `ntype::AbstractNeighborhood = FixedMassNeighborhood(1)` : The method to
   be used when evaluating the neighborhood of each reference state. See
   [`AbstractNeighborhood`](@ref) or [`neighborhood`](@ref) for more info.
@@ -50,8 +50,8 @@ immediatelly, assuming you
 have choosen sufficiently good `ks` such that the linear scaling region is bigger
 than the saturated region.
 
-The algorithm used in this function is due to Parlitz [1], which itself
-expands upon Kantz [2]. In sort, for
+The algorithm used in this function is due to Parlitz [^1], which itself
+expands upon Kantz [^2]. In sort, for
 each reference state a neighborhood is evaluated. Then, for each point in this
 neighborhood, the logarithmic distance between reference state and neighborhood
 state is
@@ -59,17 +59,17 @@ calculated as the "time" index `k` increases. The average of the above over
 all neighborhood states over all reference states is the returned result.
 
 If the `Metric` is `Euclidean()` then use the Euclidean distance of the
-full `D`-dimensional points (distance ``d_E`` in ref. [1]).
+full `D`-dimensional points (distance ``d_E`` in ref. [^1]).
 If however the `Metric` is `Cityblock()`, calculate
 the absolute distance of *only the first elements* of the `m+k` and `n+k` points
-of the reconstruction `R` (distance ``d_F`` in ref. [1]).
+of the reconstruction `R` (distance ``d_F`` in ref. [^1]).
 
 ## References
 
-[1] : Skokos, C. H. *et al.*, *Chaos Detection and Predictability* - Chapter 1
+[^1] : Skokos, C. H. *et al.*, *Chaos Detection and Predictability* - Chapter 1
 (section 1.3.2), Lecture Notes in Physics **915**, Springer (2016)
 
-[2] : Kantz, H., Phys. Lett. A **185**, pp 77–87 (1994)
+[^2] : Kantz, H., Phys. Lett. A **185**, pp 77–87 (1994)
 """
 function numericallyapunov(R::AbstractDataset{D, T}, ks;
                            refstates = 1:(length(R) - ks[end]),

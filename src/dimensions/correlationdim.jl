d6ac4f3e102abd6a6826eb4c8742fcc16da54732
George Datseris <datseris.george@gmail.com>
2020-07-09 00:08:43 +0200

    takens best estimate for correlation dimension (#119)
    * re-orgfiles
    
    * add documentation string
    
    * write takens best estoimate
    
    * test it
    
    * changelog
    
    * fix darn tests

---
+++ src/dimensions/correlationdim.jl
@@ -0,0 +1,160 @@
+#######################################################################################
+# Original correlation sum
+#######################################################################################
+using Distances
+export kernelprob, correlationsum, grassberger
+
+"""
+    kernelprob(X, ε, norm = Euclidean()) → p
+Associate each point in `X` (`Dataset` or timesries) with a probability `p` using the
+"kernel estimation" (also called "nearest neighbor kernel estimation" and other names):
+```math
+p_j = \\frac{1}{N}\\sum_{i=1}^N I(||X_i - X_j|| < \\epsilon)
+```
+where ``N`` is its length and ``I`` gives 1 if the argument is `true`.
+Because ``p`` is further normalized, it can be used as
+an alternative for the [`genentropy`](@ref) function (using the second method).
+"""
+function kernelprob(X, ε, norm = Euclidean())
+    N = length(X)
+    p = zeros(eltype(X), N)
+    @inbounds for i in 1:N
+        p[i] = count(evaluate(norm, X[i], X[j]) < ε for j in 1:N)
+    end
+    p ./= sum(p)
+    return p
+end
+
+"""
+    correlationsum(X, ε::Real; w = 1, norm = Euclidean()) → C(ε)
+Calculate the correlation sum of `X` (`Dataset` or timeseries)
+for a given radius `ε` and `norm`, using the formula:
+```math
+C(\\epsilon) = \\frac{2}{(N-w)(N-w-1)}\\sum_{i=1}^{N}\\sum_{j=1+w+i}^{N} I(||X_i - X_j|| < \\epsilon)
+```
+where ``N`` is its length and ``I`` gives 1 if the argument is `true`.
+`w` is the Theiler window, a correction to the correlation sum that skips points
+that are temporally close with each other, with the aim of removing spurious correlations.
+
+See the book "Nonlinear Time Series Analysis", Ch. 6, for a discussion
+around `w` and choosing best values.
+
+See [`grassberger`](@ref) for more.
+See also [`takens_best_estimate`](@ref).
+"""
+function correlationsum(X, ε::Real; norm = Euclidean(), w = 1)
+    N, C = length(X), 0
+    @inbounds for i in 1:N
+        C += count(evaluate(norm, X[i], X[j]) < ε for j in i+1+w:N)
+    end
+    return 2C/((N-w)*(N-1-w))
+end
+
+"""
+    correlationsum(X, εs::AbstractVector; kwargs...) → Cs
+Calculate the correlation sum for every `ε ∈ εs` using an optimized version.
+"""
+function correlationsum(X, εs::AbstractVector; norm = Euclidean(), w = 1)
+    @assert issorted(εs) "Sorted εs required for optimized version."
+    d = distancematrix(X, norm)
+    Cs = zeros(length(εs))
+    N = length(X)
+    factor = 2/((N-w)*(N-1-w))
+    for k in length(εs)÷2:-1:1
+        ε = εs[k]
+        for i in 1:N
+            @inbounds Cs[k] += count(d[j, i] < ε for j in i+1+w:N)
+        end
+        Cs[k] == 0 && break
+    end
+    for k in (length(εs)÷2 + 1):length(εs)
+        ε = εs[k]
+        for i in 1:N
+            @inbounds Cs[k] += count(d[j, i] < ε for j in i+1+w:N)
+        end
+        if Cs[k] ≈ 1/factor
+            Cs[k:end] .= 1/factor
+            break
+        end
+    end
+    return Cs .* factor
+end
+
+function distancematrix(X, norm = Euclidean())
+    N = length(X)
+    d = zeros(eltype(X), N, N)
+    @inbounds for i in 1:N
+        for j in i+1:N
+            d[j, i] = evaluate(norm, X[i], X[j])
+        end
+    end
+    return d
+end
+
+"""
+    grassberger(data, εs = estimate_boxsizes(data); kwargs...) → D_C
+Use the method of Grassberger and Proccacia[^Grassberger1983], and the correction by
+Theiler[^Theiler1986], to estimate the correlation dimension `D_C` of the given `data`.
+
+This function does something extrely simple:
+```julia
+cm = correlationsum(data, εs; kwargs...)
+return linear_region(log.(sizes), log(cm))[2]
+```
+i.e. it calculates [`correlationsum`](@ref) for various radii and then tries to find
+a linear region in the plot of the log of the correlation sum versus log(ε).
+See [`generalized_dim`](@ref) for a more thorough explanation.
+
+See also [`takens_best_estimate`](@ref).
+
+[^Grassberger1983]: Grassberger and Proccacia, [Characterization of strange attractors, PRL 50 (1983)](https://journals-aps-org.e-bis.mpimet.mpg.de/prl/abstract/10.1103/PhysRevLett.50.346)
+
+[^Theiler1986]: Theiler, [Spurious dimension from correlation algorithms applied to limited time-series data. Physical Review A, 34](https://doi.org/10.1103/PhysRevA.34.2427)
+"""
+function grassberger(data::AbstractDataset, εs = estimate_boxsizes(data); kwargs...)
+    cm = correlationsum(data, εs; kwargs...)
+    return linear_region(log.(εs), log.(cm))[2]
+end
+
+
+#######################################################################################
+# Takens' best estimate
+#######################################################################################
+export takens_best_estimate
+
+"""
+    takens_best_estimate(X, εmax, metric = Chebyshev()) → D_C
+Use the so-called "Takens' best estimate" [^Takens1985][^Theiler1987]
+method for estimating the correlation dimension
+`D_C` for the given dataset `X`.
+
+The original formula is
+```math
+D_C \\approx \\frac{C(\\epsilon_\\text{max})}{\\int_0^{\\epsilon_\\text{max}}(C(\\epsilon) / \\epsilon) \\, d\\epsilon}
+```
+where ``C`` is the [`correlationsum`](@ref) and ``\\epsilon_\\text{max}`` is an upper cutoff.
+Here we use the later expression
+```math
+D_C \\approx - \\frac{1}{\\eta},\\quad \\eta = \\frac{1}{N^*}\\sum_{[i, j]^*}\\log(||X_i - X_j|| / \\epsilon_\\text{max})
+```
+where the sum happens for all ``i, j`` so that ``i < j`` and ``||X_i - X_j|| < \\epsilon_\\text{max}``.
+
+If `X` comes from a delay coordinates embedding of a timseries `x`, a recommended value
+for ``\\epsilon_\\text{max}`` is `std(x)/4`.
+
+[^Takens1985]: Takens, On the numerical determination of the dimension of an attractor, in: B.H.W. Braaksma, B.L.J.F. Takens (Eds.), Dynamical Systems and Bifurcations, in: Lecture Notes in Mathematics, Springer, Berlin, 1985, pp. 99–106.
+[^Theiler1987]: Theiler [Efficient algorithm for estimating the correlation dimension from a set of discrete points, Phys. Rev. A 36 (9) (1987)](https://journals.aps.org/pra/abstract/10.1103/PhysRevA.36.4456)
+"""
+function takens_best_estimate(X, εmax, metric = Chebyshev())
+    n, η, N = 0, zero(eltype(X)), length(X)
+    @inbounds for i in 1:N
+        for j in i+1:N
+            d = evaluate(metric, X[i], X[j])
+            if d < εmax
+                n += 1
+                η += log(d/εmax)
+            end
+        end
+    end
+    return -n/η
+end
--- test/entropy_dimension.jl
+++ test/entropy_dimension.jl
@@ -1,6 +1,7 @@
 using ChaosTools
 using Test
 using StatsBase
+using Statistics
 
 test_value = (val, vmin, vmax) -> @test vmin <= val <= vmax
 
@@ -93,6 +94,27 @@ println("\nTesting correlation dimension...")
     es = 10 .^ range(-3, stop = 1, length = 8)
     cs = correlationsum(ts, es)
     linr, dim = linear_region(log.(es), log.(cs))
-    test_value(dim, 1.85, 2.1)
+    test_value(dim, 1.85, 2.2)
+  end
+end
+
+println("\nTesting Takens' best estimate")
+@testset "Takens best" begin
+  @testset "Henon map" begin
+    ds = Systems.henon()
+    ts = trajectory(ds, 5000)
+    x = ts[:, 1]
+    X = embed(x, 2, 1)
+    D_C = takens_best_estimate(X, std(x)/4)
+    @test 1.15 < D_C < 1.25
+  end
+  @testset "Lorenz System" begin
+    ds = Systems.lorenz()
+    tr = trajectory(ds, 2000; dt = 0.1)
+    x = tr[:, 1]
+    τ = estimate_delay(x, "mi_min", 1:20)
+    X = embed(x, 4, τ)
+    D_C = takens_best_estimate(X, std(x)/4)
+    @test 1.85 < D_C < 2.1
   end
 end

fa193dd8bd731500345e0b78999ca58ca031a338
Datseris <datseris.george@gmail.com>
2020-09-24 22:03:16 +0200

    add a first version of mean_return_times

--- src/period_return/transit_times_statistics.jl
+++ src/period_return/transit_times_statistics.jl
@@ -21,7 +21,7 @@ Return the exit and (re-)entry return times to the set(s), where each of these i
 containing all collected times for the respective `ε`-radius set, for `ε ∈ εs`.
 
 Use `transit_return(exits, entries)` to transform the output into transit and return
-times, and see also [`mean_return_time`](@ref) for both continuous and discrete systems.
+times, and see also [`mean_return_times`](@ref) for both continuous and discrete systems.
 
 ## Description
 Transit time statistics are important for the transport properties of dynamical systems[^Meiss1997]
@@ -53,7 +53,7 @@ function exit_entry_times end
 
 """
     transit_return(exits, entries) → transit, return
-Convert the output of [`return_time_statistics`](@ref) to the transit and return times.
+Convert the output of [`exit_entry_times`](@ref) to the transit and return times.
 """
 function transit_return(exits, entries)
     # the main reason this function exists is because entry times can be one less
@@ -121,17 +121,17 @@ end
 
 function exit_entry_times(integ::MDI, u0, εs, T)
     E = length(εs)
-    pre_outside = fill(false, E) # `true` if outside the ball. Previous step
-    cur_outside = copy(pre_outside)       # current step.
+    pre_outside = fill(false, E)      # `true` if outside the set. Previous step.
+    cur_outside = copy(pre_outside)   # `true` if outside the set. Current step.
     exits   = [Int[] for _ in 1:E]
     entries = [Int[] for _ in 1:E]
 
     while integ.t < T
         step!(integ)
 
-        # here i gives the index of the largest ε-ball that the trajectory is out of.
+        # here i gives the index of the largest ε-set that the trajectory is out of.
         # It is guaranteed that the trajectory is thus outside all other boxes
-        i = first_outside_index(integ, u0, εs, E) # TODO: Continuous version
+        i = first_outside_index(integ, u0, εs, E)
         cur_outside[i:end] .= true
         cur_outside[1:i-1] .= false
 
@@ -154,11 +154,64 @@ function update_exit_times!(exits, i, pre_outside, cur_outside, integ::MDI)
 end
 
 function update_entry_times!(entries, i, pre_outside, cur_outside, integ::MDI)
-    # TODO: Can I use `i` here?
+    # TODO: Can I use `i` here? I think I can do `1:i`. I can test that!
     @inbounds for j in 1:length(pre_outside)
         pre_outside[j] && !cur_outside[j] && push!(entries[j], integ.t)
     end
 end
+
+function mean_return_times(ds::DiscreteDynamicalSystem, u0, εs, T; diffeq...)
+    check_εs_sorting(εs, length(u0))
+    integ = integrator(ds, u0)
+    mean_return_times(integ, u0, εs, T)
+end
+
+function exit_entry_times(integ::MDI, u0, εs, T)
+    E = length(εs)
+    pre_outside = fill(false, E)      # `true` if outside the set. Previous step.
+    cur_outside = copy(pre_outside)   # `true` if outside the set. Current step.
+    exits, entries, counts, mrt = (zeros(Int, E) for _ in 1:4)
+
+    while integ.t < T
+        step!(integ)
+
+        # here i gives the index of the largest ε-ball that the trajectory is out of.
+        # It is guaranteed that the trajectory is thus outside all other boxes
+        i = first_outside_index(integ, u0, εs, E)
+        cur_outside[i:end] .= true
+        cur_outside[1:i-1] .= false
+
+        update_exit_times_single!(exits, i, pre_outside, cur_outside, integ)
+        update_entry_times_single!(entries, i, pre_outside, cur_outside, integ)
+        update_returns!(mrt, counts, exits, entries, i)
+        pre_outside .= cur_outside
+    end
+    return exits, entries
+end
+
+function update_exit_times_single!(exits, i, pre_outside, cur_outside, integ::MDI)
+    @inbounds for j in i:length(pre_outside)
+        cur_outside[j] && !pre_outside[j] && (exits[j] = integ.t)
+    end
+end
+
+function update_entry_times!(entries, i, pre_outside, cur_outside, integ::MDI)
+    # TODO: Can I use `i` here?
+    @inbounds for j in 1:length(pre_outside)
+        pre_outside[j] && !cur_outside[j] && (entries[j] = integ.t)
+    end
+end
+
+function update_returns!(mrt, counts, exits, entries, i)
+    for j in 1:i-1
+        if entries[j] > exits[j]
+            mrt[j] += entries[j] - exits[j]
+            counts[j] += 1
+        end
+    end
+end
+
+
 
 
 ##########################################################################################

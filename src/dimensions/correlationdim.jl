5ca9ede950659d11a8a9d62d3909a846cc79ac16
Anton <anton.paul.braun@t-online.de>
2020-10-31 16:07:11 +0100

    Further restructuring and renaming
     - `boxed_correlationsum` now handles the same as `correlationsum`.
    - `boxed_correlationdim` works just identical to `grassberger`

--- src/dimensions/correlationdim.jl
+++ src/dimensions/correlationdim.jl
@@ -2,7 +2,8 @@
 # Original correlation sum
 #######################################################################################
 using Distances, Roots
-export kernelprob, correlationsum, grassberger
+export kernelprob, correlationsum, grassberger, boxed_correlationdim, boxed_correlationsum,
+estimate_r0_buenoorovio
 
 """
     kernelprob(X, ε, norm = Euclidean()) → p
@@ -201,22 +202,28 @@ end
 # Correlationsum, but we distributed data to boxes beforehand
 ################################################################################
 """
-    boxed_correlationdim(data, r0, εs, q = 2)
-Estimates the q-order correlation dimension[^Kantz2003] out of a dataset `data` for radii `εs` by splitting the data into boxes of size `r0` beforehand. The method of splitting the data into boxes was mostly copied by the method of Theiler[^Theiler1987].
+    boxed_correlationdim(data, r0, εs; q = 2)
+Estimates the q-order correlation dimension[^Kantz2003] out of a dataset `data`
+for radii `εs` by splitting the data into boxes of size `r0` beforehand. The
+method of splitting the data into boxes was mostly copied by the method of
+Theiler[^Theiler1987].
 
-This method splits the data into boxes, calculates the q-order correlation sum C_q(ε) and fits a line through the longest linear looking region of the curve `(log(εs), log(C_q(εs)))`. The gradient of this line is the dimension.
+This method splits the data into boxes, calculates the q-order correlation sum
+C_q(ε) and fits a line through the longest linear looking region of the curve
+`(log(εs), log(C_q(εs)))`. The gradient of this line is the dimension.
 
 The function is explicitly optimized for `q = 2`.
 
-See also: [`bueno_orovio_correlationdim`](@ref), [`theiler_correlationdim`](@ref), [`correlation_boxing`](@ref), [`boxed_correlationsum`](@ref) and [`q_order_correlationsum`] (@ref)
+See also: [`bueno_orovio_correlationdim`](@ref),
+[`theiler_correlationdim`](@ref), [`correlation_boxing`](@ref),
+[`boxed_correlationsum`](@ref) and [`q_order_correlationsum`] (@ref)
 
 [^Kantz]: Kantz, H., & Schreiber, T. (2003). [More about invariant quantities. In Nonlinear Time Series Analysis (pp. 197-233). Cambridge: Cambridge University Press.](https://doi:10.1017/CBO9780511755798.013)
 
 [^Theiler1987]: Theiler, [Efficient algorithm for estimating the correlation dimension from a set of discrete points. Physical Review A, 36](https://doi.org/10.1103/PhysRevA.36.4456)
 """
-function boxed_correlationdim(data, r0, εs, q = 2)
-    boxes, contents = correlation_boxing(data, r0)
-    dd = boxed_correlationsum(boxes, contents, data, εs, q)
+function boxed_correlationdim(data, r0, εs; q = 2)
+    dd = boxed_correlationsum(data, r0, εs, q = q)
     linear_region(log.(εs), log.(dd), tol = 0.1)[2]
 end
 
@@ -227,11 +234,11 @@ Distribute `data` into boxes of size `r0`. The `q`-order correlationsum is then 
 See also: [`boxed_correlationdim`](@ref)
 """
 function boxed_correlationsum(data, r0, εs; q = 2)
-    boxes, contents = correlation_boxes(data, r0)
+    boxes, contents = correlation_boxing(data, r0)
     if q == 2
-        boxed_correlationsum_2(boxes, contents, data, ε)
+        boxed_correlationsum_2(boxes, contents, data, εs)
     else
-        boxed_correlationsum_q(boxes, contents, data, ε, q)
+        boxed_correlationsum_q(boxes, contents, data, εs, q)
     end
 end
 
@@ -274,25 +281,6 @@ function correlation_boxing(data, r0)
 end
 
 """
-    boxed_correlationsum_q(boxes, contents, data, ε, q)
-For a vector of `boxes` and the indices of their `contents` inside of `data`,
-calculate the `q`-order correlationsum of a radius or radii `ε`.
-"""
-function boxed_correlationsum_q(boxes, contents, data, ε, q)
-    q <= 1 && @warn "This function is currently not specialized for q <= 1" *
-    " and may show unexpected behaviour for these values."
-    Cs = zeros(Float64, length(ε))
-    N = length(data)
-    for index in 1:length(boxes)
-        indices = find_neighbourboxes_q(index, boxes, contents, q)
-        X = data[contents[index]]
-        Y = data[indices]
-        Cs .+= correlationsum_boxes_q(X, Y, ε, q)
-    end
-    Cs ./ (N * (N - 1) ^ (q-1))
-end
-
-"""
     boxed_correlationsum_2(boxes, contents, data, ε)
 For a vector of `boxes` and the indices of their `contents` inside of `data`,
 calculate the classic correlationsum of a radius or multiple radii `ε`.
@@ -304,7 +292,7 @@ function boxed_correlationsum_2(boxes, contents, data, ε)
         indices = find_neighbourboxes_2(index, boxes, contents)
         X = data[contents[index]]
         Y = data[indices]
-        Cs .+= correlationsum_boxes_2(X, Y, ε)
+        Cs .+= inner_correlationsum_2(X, Y, ε)
     end
     Cs .* (2 / (N * (N - 1)))
 end
@@ -326,6 +314,49 @@ function find_neighbourboxes_2(index, boxes, contents)
 end
 
 """
+    inner_correlationsum_2(X, Y, εs; norm = Euclidean())
+Calculates the classic correlation sum for values `X` inside a box, considering `Y` consisting of all values in that box and the ones in neighbouring boxes for all distances `ε ∈ εs` calculated by `norm`.
+
+See also: [`correlationsum`](@ref)
+"""
+function inner_correlationsum_2(X, Y, εs; norm = Euclidean())
+    @assert issorted(εs) "Sorted εs required for optimized version."
+    Cs, Ny, Nε = zeros(length(εs)), length(Y), length(εs)
+    for (i, x) in enumerate(X)
+        for j in i+1:Ny
+            dist = evaluate(norm, Y[j], x)
+            for k in Nε:-1:1
+                if dist < εs[k]
+                    Cs[k] += 1
+                else
+                    break
+                end
+            end
+        end
+    end
+    return Cs
+end
+
+"""
+    boxed_correlationsum_q(boxes, contents, data, ε, q)
+For a vector of `boxes` and the indices of their `contents` inside of `data`,
+calculate the `q`-order correlationsum of a radius or radii `ε`.
+"""
+function boxed_correlationsum_q(boxes, contents, data, ε, q)
+    q <= 1 && @warn "This function is currently not specialized for q <= 1" *
+    " and may show unexpected behaviour for these values."
+    Cs = zeros(Float64, length(ε))
+    N = length(data)
+    for index in 1:length(boxes)
+        indices = find_neighbourboxes_q(index, boxes, contents, q)
+        X = data[contents[index]]
+        Y = data[indices]
+        Cs .+= inner_correlationsum_q(X, Y, ε, q)
+    end
+    Cs ./ (N * (N - 1) ^ (q-1))
+end
+
+"""
     find_neighbourboxes_q(index, boxes, contents, q) → indices
 For an `index` into `boxes` all neighbouring boxes are searched. If the found
 box is indeed a neighbour, the `contents` of that box are added to `indices`.
@@ -342,12 +373,12 @@ function find_neighbourboxes_q(index, boxes, contents, q)
 end
 
 """
-    correlationsum_boxes(q::Real, X, Y, εs; norm = Euclidean())
+    inner_correlationsum_q(q::Real, X, Y, εs; norm = Euclidean())
 Calculates the `q`-order correlation sum for values `X` inside a box, considering `Y` consisting of all values in that box and the ones in neighbouring boxes for all distances `ε ∈ εs` calculated by `norm`.
 
 See also: [`correlationsum`](@ref)
 """
-function correlationsum_boxes_q(X, Y, εs, q::Real; norm = Euclidean())
+function inner_correlationsum_q(X, Y, εs, q::Real; norm = Euclidean())
     @assert issorted(εs) "Sorted εs required for optimized version."
     Cs, Ny, Nε = zeros(length(εs)), length(Y), length(εs)
     for (i, x) in enumerate(X)
@@ -368,33 +399,8 @@ function correlationsum_boxes_q(X, Y, εs, q::Real; norm = Euclidean())
     return Cs
 end
 
-
 """
-    correlationsum_boxes_2(X, Y, εs; norm = Euclidean())
-Calculates the classic correlation sum for values `X` inside a box, considering `Y` consisting of all values in that box and the ones in neighbouring boxes for all distances `ε ∈ εs` calculated by `norm`.
-
-See also: [`correlationsum`](@ref)
-"""
-function correlationsum_boxes_2(X, Y, εs; norm = Euclidean())
-    @assert issorted(εs) "Sorted εs required for optimized version."
-    Cs, Ny, Nε = zeros(length(εs)), length(Y), length(εs)
-    for (i, x) in enumerate(X)
-        for j in i+1:Ny
-            dist = evaluate(norm, Y[j], x)
-            for k in Nε:-1:1
-                if dist < εs[k]
-                    Cs[k] += 1
-                else
-                    break
-                end
-            end
-        end
-    end
-    return Cs
-end
-
-"""
-	estimate_r0_theiler(data)
+    estimate_r0_theiler(data)
 Estimates a reasonable size for boxing the data before calculating the correlation dimension proposed by Theiler[^Theiler1987].
 To do so the dimension is estimated by running the algorithm by Grassberger and Procaccia[^Grassberger1983] with `√N` points where `N` is the number of total data points. Then the optimal boxsize ``r_0`` computes as
 ```math
@@ -407,23 +413,23 @@ where ``R`` is the size of the chaotic attractor and ``\\nu`` is the estimated d
 [^Grassberger1983]: Grassberger and Proccacia, [Characterization of strange attractors, PRL 50 (1983)](https://journals-aps-org.e-bis.mpimet.mpg.de/prl/abstract/10.1103/PhysRevLett.50.346)
 """
 function estimate_r0_theiler(data)
-	N = length(data)
-	mini, maxi = minmaxima(data)
-	R = maximum(maxi .- mini)
-	# Sample √N datapoints for a rough estimate of the dimension.
-	data_sample = data[unique(rand(1:N, ceil(Int, sqrt(N))))] |> Dataset
-	# Define radii for the rough dimension estimate
-	lower = log10(min_pairwise_distance(data_sample)[2])
-	εs = 10 .^ range(lower, stop = log10(R), length = 12)
-	# Actually estimate the dimension.
-	cm = correlationsum(data_sample, εs)
-	ν = linear_region(log.(εs), log.(cm), tol = 0.5)[2]
-	# The combination yields the optimal box size
-	r0 = R * (2/N)^(1/ν)
+    N = length(data)
+    mini, maxi = minmaxima(data)
+    R = maximum(maxi .- mini)
+    # Sample √N datapoints for a rough estimate of the dimension.
+    data_sample = data[unique(rand(1:N, ceil(Int, sqrt(N))))] |> Dataset
+    # Define radii for the rough dimension estimate
+    lower = log10(min_pairwise_distance(data_sample)[2])
+    εs = 10 .^ range(lower, stop = log10(R), length = 12)
+    # Actually estimate the dimension.
+    cm = correlationsum(data_sample, εs)
+    ν = linear_region(log.(εs), log.(cm), tol = 0.5)[2]
+    # The combination yields the optimal box size
+    r0 = R * (2/N)^(1/ν)
 end
 
 """
-	estimate_r0_buenoorovio(data)
+    estimate_r0_buenoorovio(data)
 Estimates a reasonable size for boxing the data proposed by Bueno and Orovio[^Bueno2007] before calculating the correlation dimension as presented by Theiler[^Theiler1983].
 To do so the dimension `ν` is estimated by running the algorithm by Grassberger and Procaccia[^Grassberger1983] with `√N` points where `N` is the number of total data points.
 An effective size `ℓ` of the attractor is calculated by boxing a small subset of size `N/10` into boxes of sidelength `r_ℓ` and counting the number of filled boxes `η_ℓ`.
@@ -447,27 +453,33 @@ r_0 = \\ell / \\eta_\\textrm{opt}^{1/\\nu}.
 [^Grassberger1983]: Grassberger and Proccacia, [Characterization of strange attractors, PRL 50 (1983)](https://journals-aps-org.e-bis.mpimet.mpg.de/prl/abstract/10.1103/PhysRevLett.50.346)
 """
 function estimate_r0_buenoorovio(data)
+    r0 = zero(eltype(data))
     mini, maxi = minmaxima(data)
     N = length(data)
     m = DelayEmbeddings.dimension(data)
-	R = maximum(maxi .- mini)
-	# Sample √N and N/10 datapoints out of data for rough estimates.
-	data_sample1 = data[unique(rand(1:N, ceil(Int, sqrt(N))))] |> Dataset
-	data_sample2 = data[unique(rand(1:N, N÷10))] |> Dataset
-	# Define logarithmic shaped vector of radii.
-	lower = log10(min_pairwise_distance(data_sample1)[2])
-	εs = 10 .^ range(lower, stop = log10(R), length = 16)
-	# Estimating ν out of a small sample using the Grassberger Procaccia algorithm.
-	cm = correlationsum(data_sample1, εs)
-	ν = linear_region(log.(εs), log.(cm), tol = 0.5)[2]
-	# Calculate the optimal number of filled boxes according to Bueno and Orovio.
-	η_opt = N^(2/3) * ((3^ν - 1/2) / (3^m - 1))^(1/2)
-	r_ℓ = R / 10
-	η_ℓ = length(correlation_boxing(data_sample2, r_ℓ)[1])
-	# Estimate the effictive size of the chaotic attractor.
-	ℓ = r_ℓ * η_ℓ^(1/ν)
-	# The optimal box size is the effictive size divided by the box number to the
-	# power of the inverse dimension.
+    R = maximum(maxi .- mini)
+    # The possibility of a bad pick exists, if so, the calculation is repeated.
+    ν = zero(eltype(data))
+    while true
+        # Sample √N datapoints for rough dimension estimate
+        data_sample1 = data[unique(rand(1:N, ceil(Int, sqrt(N))))] |> Dataset
+        # Define logarithmic series of radii.
+        lower = log10(min_pairwise_distance(data_sample1)[2])
+        εs = 10 .^ range(lower, stop = log10(R), length = 16)
+        # Estimate ν from a sample using the Grassberger Procaccia algorithm.
+        cm = correlationsum(data_sample1, εs)
+        ν = linear_region(log.(εs), log.(cm), tol = 0.5)[2]
+        !iszero(ν) && break
+    end
+    # Sample N/10 datapoints out of data for rough estimate of effective size.
+    data_sample2 = data[unique(rand(1:N, N÷10))] |> Dataset
+    r_ℓ = R / 10
+    η_ℓ = length(correlation_boxing(data_sample2, r_ℓ)[1])
+    # Estimate the effictive size of the chaotic attractor.
+    ℓ = r_ℓ * η_ℓ^(1/ν)
+    # Calculate the optimal number of filled boxes according to Bueno-Orovio
+    η_opt = N^(2/3) * ((3^ν - 1/2) / (3^m - 1))^(1/2)
+    # The optimal box size is the effictive size divided by the box number # to the power of the inverse dimension.
     r0 = ℓ / η_opt^(1/ν)
 end
 

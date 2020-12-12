6304a0f5cc3f0386d9fc7110f58c97454bc06d0e
Datseris <datseris.george@gmail.com>
2020-12-12 21:21:24 +0100

    move linear regions to separate file

---
+++ src/dimensions/linear_regions.jl
@@ -0,0 +1,131 @@
+#=
+this file includes functionality for estimating linear scaling regions and
+defines the `generalized_dim` function.
+=#
+
+export linear_region, linear_regions, estimate_boxsizes, linreg
+export boxcounting_dim, capacity_dim, generalized_dim,
+information_dim, estimate_boxsizes, kaplanyorke_dim
+export molteno_dim, molteno_boxing
+#####################################################################################
+# Functions and methods to deduce linear scaling regions
+#####################################################################################
+using Statistics
+using Statistics: covm, varm
+# The following function comes from a version in StatsBase that is now deleted
+# StatsBase is copyrighted under the MIT License with
+# Copyright (c) 2012-2016: Dahua Lin, Simon Byrne, Andreas Noack, Douglas Bates,
+# John Myles White, Simon Kornblith, and other contributors.
+"""
+    linreg(x, y) -> a, b
+Perform a linear regression to find the best coefficients so that the curve:
+`y = a + b*x` has the least squared error.
+"""
+function linreg(x::AbstractVector, y::AbstractVector)
+    # Least squares given
+    # Y = a + b*X
+    # where
+    # b = cov(X, Y)/var(X)
+    # a = mean(Y) - b*mean(X)
+    if size(x) != size(y)
+        throw(DimensionMismatch("x has size $(size(x)) and y has size $(size(y)), " *
+            "but these must be the same size"))
+    end
+    mx = Statistics.mean(x)
+    my = Statistics.mean(y)
+    # don't need to worry about the scaling (n vs n - 1)
+    # since they cancel in the ratio
+    b = covm(x, mx, y, my)/varm(x, mx)
+    a = my - b*mx
+    return a, b
+end
+
+slope(x, y) = linreg(x, y)[2]
+
+"""
+    linear_region(x, y; dxi::Int = 1, tol = 0.2) -> ([ind1, ind2], slope)
+Call [`linear_regions`](@ref), identify the largest linear region
+and approximate the slope of the entire region using `linreg`.
+Return the indices where
+the region starts and stops (`x[ind1:ind2]`) as well as the approximated slope.
+"""
+function linear_region(x::AbstractVector, y::AbstractVector;
+    dxi::Int = 1, tol::Real = 0.2)
+
+    # Find biggest linear region:
+    reg_ind = max_linear_region(linear_regions(x,y; dxi=dxi, tol=tol)...)
+    # least squares fit:
+    xfit = view(x, reg_ind[1]:reg_ind[2])
+    yfit = view(y, reg_ind[1]:reg_ind[2])
+    approx_tang = slope(xfit, yfit)
+    return reg_ind, approx_tang
+end
+
+"""
+    max_linear_region(lrs::Vector{Int}, tangents::Vector{Float64})
+Find the biggest linear region and return it.
+"""
+function max_linear_region(lrs::Vector{Int}, tangents::Vector{Float64})
+    dis = 0
+    tagind = 0
+    for i in 1:length(lrs)-1
+        if lrs[i+1] - lrs[i] > dis
+            dis = lrs[i+1] - lrs[i]
+            tagind = i
+        end
+    end
+    return [lrs[tagind], lrs[tagind+1]]
+end
+
+"""
+    linear_regions(x, y; dxi::Int = 1, tol = 0.2) -> (lrs, tangents)
+Identify regions where the curve `y(x)` is linear, by scanning the
+`x`-axis every `dxi` indices (e.g. at `x[1] to x[5], x[5] to x[10], x[10] to x[15]`
+and so on if `dxi=5`).
+
+If the slope (calculated via linear regression) of a region of width `dxi` is
+approximatelly equal to that of the previous region,
+within tolerance `tol`,
+then these two regions belong to the same linear region.
+
+Return the indices of `x` that correspond to linear regions, `lrs`,
+and the approximated `tangents` at each region. `lrs` is a vector of `Int`.
+Notice that `tangents` is _not_ accurate: it is not recomputed at every step,
+but only when its error exceeds the tolerance `tol`! Use [`linear_region`](@ref)
+to obtain a correct estimate for the slope of the largest linear region.
+"""
+function linear_regions(x::AbstractVector, y::AbstractVector;
+    dxi::Int = 1, tol::Real = 0.2)
+
+    maxit = length(x) ÷ dxi
+
+    tangents = Float64[slope(view(x, 1:max(dxi, 2)), view(y, 1:max(dxi, 2)))]
+
+    prevtang = tangents[1]
+    lrs = Int[1] #start of first linear region is always 1
+    lastk = 1
+
+    # Start loop over all partitions of `x` into `dxi` intervals:
+    for k in 1:maxit-1
+        # tang = linreg(view(x, k*dxi:(k+1)*dxi), view(y, k*dxi:(k+1)*dxi))[2]
+        tang = slope(view(x, k*dxi:(k+1)*dxi), view(y, k*dxi:(k+1)*dxi))
+        if isapprox(tang, prevtang, rtol=tol)
+            # Tanget is similar with initial previous one (based on tolerance)
+            continue
+        else
+            # Tangent is not similar.
+            # Push new tangent for a new linear region
+            push!(tangents, tang)
+
+            # Set the START of a new linear region
+            # which is also the END of the previous linear region
+            push!(lrs, k*dxi)
+            lastk = k
+        end
+
+        # Set new previous tangent (only if it was not the same as current)
+        prevtang = tang
+    end
+    push!(lrs, length(x))
+    return lrs, tangents
+end

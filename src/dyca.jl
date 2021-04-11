f658ace711d015a11fba7a2c14acbd0896dbba94
Anantha S Rao <aanantha.s.rao@gmail.com>
2021-04-11 19:12:26 +0530

    docstring edit

--- src/dyca.jl
+++ src/dyca.jl
@@ -2,21 +2,27 @@ using LinearAlgebra
 
 """
     matrix_gradient(matrix::Matrix{Float64})
-Compute the gradient of 2-dimensional array using second order accurate central differences in the interior points and either first order accurate one-sides (forward or backwards) differences at the boundaries. The returned gradient hence has the same shape as the input array. Here, we compute the gradient along axis=1 (row-wise). To compute gradient along axis=2 (column-wise), the input must be the tranpose of the matrix. 
-We find the standard second order approximation by using: 
-```math
-\\hat{f}_i^{(1)} = \\frac{f(x_{i+1}-f(x_{i-1})}{2h} + O(h^2)
-```
+Compute the gradient of a 2-dimensional array along axis=1
 
 
 ## Arguments
-- `matrix::Matrix{Number}` : The input matrix with two dimensions, entries can be real or complex
+- `matrix::Matrix{Float64}` : The input matrix with two dimensions, with real entries. 
+
+## Returns 
+- `gradient::Matrix{Float64}` : The gradient of the input matrix found along axis=1
+
+## Description
+Compute the gradient using second order accurate central differences in the interior points and first order accurate one-sides differences at the boundaries. We find the standard second order approximation by using: 
+```math
+\\hat{f}_i^{(1)} = \\frac{f(x_{i+1}-f(x_{i-1})}{2h} + O(h^2)
+```
+The returned gradient matrix hence has the same shape as the input array. Here we compute the gradient along axis=1 (row-wise), so to compute gradient along axis=2 (column-wise), the tranpose of the input matrix must be given.  
 
 ## Example: 
 
 ```julia
 random_array = rand(2,22;8,8);
-matrix_fdm_gradient(random_array,1)
+matrix_gradient(random_array)
 ```
 
 [^Quarteroni2007]: Quarteroni A., Sacco R., Saleri F. (2007) Numerical Mathematics (Texts in Applied Mathematics). New York: Springer.
@@ -31,29 +37,33 @@ function matrix_gradient(matrix::Matrix{Float64})
 end
 
 """
-    dyca(data::Array,eig_thresold::float64)
-Computes the Dynamical Component analysis matrix [^Uhl2018] used for dimensionality reduction. Here, we solve the generalised eigenvalue equation: 
+    dyca(data::Matrix{Float64},eig_thresold::Float64)
+Compute the Dynamical Component analysis (DyCA) matrix [^Uhl2018] used for dimensionality reduction by solving the generalised eigenvalue equation.
+
+
+## Arguments
+- `data::Matrix{float64}` : The input matrix with size= (2,2)
+- `eig_thresold::float64` : The eigenvalue thresold for DyCA
+
+## Returns
+- Eigenvalues of the Generalised eigenvalue problem
+- The Projection matrix
+- Data of the reduced dimension obtained by the projection matrix
+
+## Description 
+Here, we solve the generalised eigenvalue equation: 
 ```math
 C_1 C_0^{-1} C_1^{\\top} \\bar{u} = \\lambda C_2 \\bar{u}
 
 ```
 where ``C_0`` is the correlation matrix of the signal with itself, ``C_1`` the correlation matrix of the signal with its derivative, and ``C_2`` the correlation matrix of the derivative of the data with itself. The eigenvectors ``\\bar{u}`` to eigenvalues approximately 1 and their ``C_1^{-1} C_2 u`` counterpart form the space where to project onto. 
 
-## Arguments
-- `data::Array{float64}` : The input matrix with size= (2,2)
-- `eig_thresold::float64` : the eigenvalue thresold for DyCA
-
-## Returns
-- Eigenvalues of the Generalised eigenvalue problem
-- Projection matrix
-- Data embeded by the projection matrix
-
 ## Example: 
 
 ```julia
 random_array = rand(2,22;100,100);
-eigen_thresold = 0.8 ;
-DyCA(random_array,eigen_thresold)
+eigen_thresold = 0.90 ;
+eigenvalues, proj_matrix, proj_data = dyca(random_array,eigen_thresold)
 ```
 
 [^Uhl2018]: B Seifert, K Korn, S Hartmann, C Uhl, *Dynamical Component Analysis (DYCA): Dimensionality Reduction for High-Dimensional Deterministic Time-Series*, 10.1109/mlsp.2018.8517024, 2018 IEEE 28th International Workshop on Machine Learning for Signal Processing (MLSP)
@@ -82,5 +92,5 @@ function dyca(data,eig_thresold::Float64)
         throw(DomainError("No generalized eigenvalue fulfills threshold!"))
     end
     
-     return eigenvalues, proj_mat, data * proj_mat
+     return eigenvalues, proj_mat, data*proj_mat
 end

7cec4d6483a3d44cfcd20498da849c9504b5c1fc
apbraun <anton.paul.braun@t-online.de>
2021-04-14 21:50:53 +0200

    Update docs

--- src/dimensions/fixedmass.jl
+++ src/dimensions/fixedmass.jl
@@ -3,7 +3,7 @@ export correlationsum_fixedmass
 using SpecialFunctions, Random
 
 """
-    correlationsum_fixedmass(data, max_j; metric = Euclidean(), M = length(data)) → rs, ϵs
+    correlationsum_fixedmass(data, max_j; metric = Euclidean(), M = length(data)) → rs, ys
 A fixed mass algorithm for the calculation of the fractal dimension according
 to [^Grassberger 1988] with `max_j` the maximum number of neighbours that
 should be considered for the calculation, `M` defines the number of points
@@ -13,15 +13,16 @@ Implements
 ```math
 D_q \\overline{\\log r^{(j)}} \\sim Ψ(j) - \\log N
 ```
-where `` Ψ(j) = \\frac{\\text{d} \\log Γ(j)}{\\text{d} j}
-``, `rs` = ``\\overline{\\log r^{(j)}}`` and `ϵs` = ``Ψ(j) - \\log N``.
+where `` \\Psi(j) = \\frac{\\text{d} \\log Γ(j)}{\\text{d} j}
+``, `rs` = ``\\overline{\\log r^{(j)}}`` and `ys` = ``\\Psi(j) - \\log N``.
 
-``D_q`` can be computed by using `linear_region(rs, ϵs)`.
+``D_q`` can be computed by using `linear_region(rs, ys)`.
 
 [^Grassberger 1988]: Peter Grassberger (1988) [Finite sample Corrections to Entropy and Dimension Estimates, Physics Letters A 128(6-7)](https://doi.org/10.1016/0375-9601(88)90193-4)
 """
 function correlationsum_fixedmass(data, max_j; metric = Euclidean(), M = length(data), w = 0)
     N = length(data)
+    M > N && throw(ArgumentError("The number of points used for the calculation `M` should not exceed the number of points provided."))
     # Transform the data into a tree.
     tree = searchstructure(KDTree, data, metric)
     # Calculate all nearest neighbours up to max_j.

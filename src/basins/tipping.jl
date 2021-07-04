76f32d5828f85ad608701047045921dff11bce0a
Datseris <datseris.george@gmail.com>
2021-07-04 19:45:08 +0200

    fix docstring of tipping probabilities

--- src/basins/tipping.jl
+++ src/basins/tipping.jl
@@ -26,11 +26,10 @@ end
 """
     tipping_probabilities(basins_before, basins_after) → P
 Return the tipping probabilities of the computed basins before and after a change
-in the system parameters (or time forcing), according to the definition of ^..
+in the system parameters (or time forcing), according to the definition of[^Kaszás2019].
 
-The input `basins` are integer-valued arrays, where the integers enumerate the attractor.
-They can be of any dimensionality provided that `size(basins_before) == size(basins_after).
-Typically they are 2D, as the output of [`basins_2D`](@ref) or [`basins_general`](@ref)
+The input `basins` are integer-valued arrays, where the integers enumerate the attractor,
+e.g. the output of [`basins_of_attraction`](@ref).
 
 ## Description
 Let ``\\mathcal{B}_i(p)`` denote the basin of attraction of attractor ``A_i`` at

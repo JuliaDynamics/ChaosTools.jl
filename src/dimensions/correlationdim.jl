315561e7673d4cb185ebc74b0c3f38de9480aa88
Datseris <datseris.george@gmail.com>
2021-08-18 14:48:25 +0300

    imrpove docstring of boxed_cor

--- src/dimensions/correlationdim.jl
+++ src/dimensions/correlationdim.jl
@@ -177,26 +177,29 @@ end
 # Correlationsum, but we distributed data to boxes beforehand
 ################################################################################
 """
-    boxed_correlationsum(data, εs, r0 = maximum(εs); q = 2 , M = size(data, 2), w = 0)
-    boxed_correlationsum(data; q = 2 , M = size(data, 2), w = 0)
+    boxed_correlationsum(data, εs, r0 = maximum(εs); q=2 , M=dimension(data), w=0) → Cs
 
-Estimate the box assisted q-order correlation sum[^Kantz2003] out of a
-dataset `data` for radii `εs` by splitting the data into boxes of size `r0`
+Estimate the box assisted q-order correlation sum[^Kantz2003] `Cs` out of a
+dataset `data` for each radius in `εs`, by splitting the data into boxes of size `r0`
 beforehand.
+
+This method is much faster than [`correlationsum`](@ref), **provided that** the 
+box size `r0` is significantly smaller than then the attractor length.
+A good estimate for `r0` is [`estimate_r0_buenoorovio`](@ref).
+
+    boxed_correlationsum(data; q = 2 , M = size(data, 2), w = 0) → εs, Cs
+
+In this method the minimum inter-point distance and [`estimate_r0_buenoorovio`](@ref)
+are used to estimate good `εs` for the calculation, which are also returned.
 
 ## Description
 `C_q(ε)` is calculated for every `ε ∈ εs` and each of the boxes to then be
 summed up afterwards. If `M` is unequal to the dimension of the data, only the
 first `M` dimensions are considered for the box distribution (this is called the
-prism-assisted version).
-The method of splitting the data into boxes was
-implemented according to Theiler[^Theiler1987]. If only a dataset is given the
-radii `εs` and boxsize `r0` are chosen by calculating
-[`estimate_r0_buenoorovio`](@ref).
+prism-assisted version). The method of splitting the data into boxes was 
+implemented according to Theiler[^Theiler1987]. `w` is the [Theiler window](@ref).
 
-`w` is the [Theiler window](@ref).
-
-The function is explicitly optimized for `q = 2` and becomes quite slow for `q ≠ 2`.
+The function is explicitly optimized for `q = 2` but becomes quite slow for `q ≠ 2`.
 
 See [`correlationsum`](@ref) for the definition of `C_q`
 and also [`data_boxing`](@ref).
@@ -209,7 +212,8 @@ function boxed_correlationsum(data; kwargs...)
     r0 = estimate_r0_buenoorovio(data, M)
     ε0 = minimum_pairwise_distance(data)[1]
     εs = 10 .^ range(log10(ε0), log10(r0), length = 16)
-    boxed_correlationsum(data, εs, r0; kwargs...)
+    Cs = boxed_correlationsum(data, εs, r0; kwargs...)
+    return εs, Cs
 end
 
 function boxed_correlationsum(

ac9f53bbe90b28ecf30288af877249313e4c51c2
Datseris <datseris.george@gmail.com>
2021-09-27 23:04:00 +0200

    move basin fractions to a new file

---
+++ src/basins/basins_fractions.jl
@@ -0,0 +1,24 @@
+export basin_fractions
+
+
+"""
+    basin_fractions(basins::Array) → fs::Dict
+Calculate the fraction of the basins of attraction encoded in `basins`.
+The elements of `basins` are integers, enumerating the attractor that the entry of `basins`
+converges to. Return a dictionary that maps attractor IDs to their relative fractions.
+
+In[^Menck2013] the authors use these fractions to quantify the stability of a basin of
+attraction, and specifically how it changes when a parameter is changed.
+
+[^Menck2013]: Menck, Heitzig, Marwan & Kurths. How basin stability complements the linear stability paradigm. [Nature Physics, 9(2), 89–92](https://doi.org/10.1038/nphys2516)
+"""
+function basin_fractions(basins::AbstractArray)
+    fs = Dict{eltype(basins), Float64}()
+    ids = unique(basins)
+    N = length(basins)
+    for ξ in ids
+        B = count(isequal(ξ), basins)
+        fs[ξ] = B/N
+    end
+    return fs
+end

3b43b0c98ec07d972b4d8b0ed47357763570f413
kalelr <kalelluizrossi@gmail.com>
2021-09-28 19:37:51 -0300

    oops, fixed mistake my mistake in runtests

--- test/runtests.jl
+++ test/runtests.jl
@@ -10,7 +10,7 @@ ti = time()
 include("basins/basins_tests.jl")
 include("basins/uncertainty_tests.jl")
 include("basins/tipping_points_tests.jl")
-include("basins/basins_tests.jl")
+include("basins/stability_basin_tests.jl")
 
 include("orbitdiagrams/orbitdiagram_tests.jl")
 include("orbitdiagrams/poincare_tests.jl")

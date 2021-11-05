6812d9b7ec2954ddbca828773951448276a67a6c
Kalel Luiz Rossi <38593444+KalelR@users.noreply.github.com>
2021-11-05 20:23:27 -0300

    Update on lyapunov convergence (#226)
    * Updated lyapunovspectrum_convergence for continuous systems, including the more performant buffered QR; included the function for discrete systems
    
    * Added test for lyapunovspectrum_convergence for discrete systems
    
    * Incremented patch version and added description in CHANGELOG
    
    Co-authored-by: George Datseris <datseris.george@gmail.com>

--- src/chaosdetection/lyapunovs.jl
+++ src/chaosdetection/lyapunovs.jl
@@ -145,34 +145,103 @@ function lyapunovspectrum(ds::DDS{false, T, 1}, N; Ttr = 0) where {T}
     return λ/N
 end
 
-function lyapunovspectrum_convergence(integ, N, dt::Real, Ttr::Real = 0.0)
+#####################################################################################
+#                           Lyapunov Spectrum Convergence                           #
+#####################################################################################
+"""
+    lyapunovspectrum_convergence(ds::DynamicalSystem, N [, k::Int | Q0]; kwargs...) -> λs, t
 
-    T = stateeltype(integ)
-    t0 = integ.t
+Computes the convergence of the Lyapunov exponents, calculated exactly as in `lyapunovspectrum`.
+(See [`lyapunovspectrum`](@ref) for information on the parameters and implementation).
+Returns `λs`, a vector of `N` vectors, wherein vector of index `i` contains the Lyapunov
+spectrum evaluated up to the time `t[i]`. The final vector is equal to the returned vector
+of `lyapunovspectrum`.
+
+## Performance Notes
+This function uses a [`tangent_integrator`](@ref).
+For loops over initial conditions and/or
+parameter values one should use the low level method that accepts
+an integrator, and `reinit!` it to new initial conditions.
+See the "advanced documentation" for info on the integrator object.
+The low level method is
+```julia
+lyapunovspectrum_convergence(tinteg, N, Δt::Real, Ttr::Real)
+```
+"""
+lyapunovspectrum_convergence(ds::DS, N, k::Int = dimension(ds); kwargs...) =
+lyapunovspectrum_convergence(ds, N, orthonormal(dimension(ds), k); kwargs...)
+
+function lyapunovspectrum_convergence(ds::DS{IIP, S, D}, N, Q0::AbstractMatrix;
+        Ttr::Real = 0, Δt::Real = 1, u0 = get_state(ds), show_progress = false, diffeq...
+    ) where {IIP, S, D}
+
+    if typeof(ds) <: DDS
+        @assert typeof(Ttr) == Int
+        integ = tangent_integrator(ds, Q0; u0)
+    else
+        integ = tangent_integrator(ds, Q0; u0, diffeq...)
+    end
+    λ = lyapunovspectrum_convergence(integ, N, Δt, Ttr, show_progress)
+    return λ
+end
+
+function lyapunovspectrum_convergence(integ, N, Δt::Real, Ttr::Real = 0.0, show_progress=false)
+    if show_progress
+        progress = ProgressMeter.Progress(N; desc = "Lyapunov Spectrum: ", dt = 1.0)
+    end
+    B = copy(get_deviations(integ)) # for use in buffer
     if Ttr > 0
+        t0 = integ.t
         while integ.t < t0 + Ttr
-            step!(integ, dt)
-            qrdec = LinearAlgebra.qr(get_deviations(integ))
-            set_deviations!(integ, qrdec.Q)
+            step!(integ, Δt)
+            Q, R = _buffered_qr(B, get_deviations(integ))
+            set_deviations!(integ, Q)
         end
     end
+
     k = size(get_deviations(integ))[2]
+    T = stateeltype(integ)
     t0 = integ.t; t = zeros(T, N); t[1] = t0
     λs = [zeros(T, k) for i in 1:N];
 
     for i in 2:N
-        step!(integ, dt)
-        qrdec = LinearAlgebra.qr(get_deviations(integ))
+        step!(integ, Δt)
+        Q, R = _buffered_qr(B, get_deviations(integ))
         for j in 1:k
-            @inbounds λs[i][j] = λs[i-1][j] + log(abs(qrdec.R[j,j]))
+            @inbounds λs[i][j] = λs[i-1][j] + log(abs(R[j,j]))
         end
         t[i] = integ.t
-        set_deviations!(integ, qrdec.Q)
+        set_deviations!(integ, Q)
+        show_progress && ProgressMeter.update!(progress, i)
     end
     popfirst!(λs); popfirst!(t)
     for j in eachindex(t)
         λs[j] ./= (t[j] - t0)
     end
+    return λs, t
+end
+
+lyapunovspectrum_convergence(ds::DiscreteDynamicalSystem{IIP, T, 1}, a...; kw...) where {IIP, T} =
+error("For discrete 1D systems, only method with state type = number is implemented.")
+
+function lyapunovspectrum_convergence(ds::DDS{false, T, 1}, N; Ttr = 0) where {T}
+    x = get_state(ds); f = ds.f
+    p = ds.p; t0 = ds.t0
+    if Ttr > 0
+        for i in t0:(Ttr+t0)
+            x = f(x, p, i)
+        end
+    end
+    t = (t0+Ttr):(t0+Ttr+N)
+    λs = zeros(T, length(t));
+    for idx = 2:length(t)
+        i = t[idx]
+        x = f(x, p, i)
+        @inbounds λs[idx] = λs[idx-1] + log(abs(ds.jacobian(x, p, i)))
+    end
+    t = collect(t)
+    popfirst!(λs); popfirst!(t)
+    λs ./= t .- t0
     return λs, t
 end
 

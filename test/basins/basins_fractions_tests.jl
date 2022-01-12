8d333917cea29642aa7e3c26d462d0031ef0ccbe
kalelr <kalelluizrossi@gmail.com>
2022-01-12 11:41:06 +0100

    Update tests with new 'interface'

--- test/basins/basins_fractions_tests.jl
+++ test/basins/basins_fractions_tests.jl
@@ -13,7 +13,6 @@ using ChaosTools, DelayEmbeddings, DynamicalSystemsBase
 
     function feature_extraction(t, y, extract_params=NamedTuple())
         # Ttr = extract_params.Ttr;
-        # idx_steady = findall(x->x>Ttr, t) #I don't think we need to do this since the integration already takes the transients out
         Δ = abs(maximum(y[:, 2]) - mean(y[:,2])) #"spread" of vaqlues, how far max and mean are
         X = zeros(2)
         if(Δ < 0.01) #mean ~ max -> FP
@@ -43,7 +42,6 @@ using ChaosTools, DelayEmbeddings, DynamicalSystemsBase
     sampling_method = "uniform"; # sampling strategy / probability density
 
     s = ChaosTools.sampler(min_bounds=min_limits, max_bounds=max_limits, method=sampling_method)
-    Random.seed!(1)
     ics_foo = s
     ics = [s() for i=1:N]
     ics = Dataset(ics)
@@ -51,28 +49,28 @@ using ChaosTools, DelayEmbeddings, DynamicalSystemsBase
 
     #---Running for supervised
     #prepare templates
-    ic_templates = Dataset([0.5 0; 2.7 0]) #each IC along a row
-    templates_labels = ["IC", "LC"]
+    attractors_ic = Dataset([0.5 0; 2.7 0]) #each IC along a row
 
-    # println("Test No1. Supervised")
-    S, class_labels = basin_fractions(ds, feature_extraction, ics, ic_templates; T=Texec, Ttr=Ttr, Δt=Δt)
-    # S = (Dict(2 => 0.8452,1 => 0.1548)
-    @test 0.12 < S[1] < 0.21
-    @test 0.79 < S[2] < 0.88
-
-
-    S, class_labels = basin_fractions(ds, feature_extraction, ics_foo, ic_templates; num_samples=N, T=Texec, Ttr=Ttr, Δt=Δt)
-    # S = (Dict(2 => 0.8452,1 => 0.1548)
-    @test 0.12 < S[1] < 0.21
-    @test 0.79 < S[2] < 0.88
+    # println("Test No. 1 Supervised, generated ics.")
+    fs, class_labels = basin_fractions(ds, feature_extraction, ics, attractors_ic; T=Texec, Ttr=Ttr, Δt=Δt)
+    # fs = (Dict(2 => 0.8452,1 => 0.1548)
+    @test 0.12 < fs[1] < 0.21
+    @test 0.79 < fs[2] < 0.88
 
 
-    # println("Test No2. Unsupervised")
-    S, class_labels = basin_fractions(ds, feature_extraction, ics; T=Texec, Ttr=Ttr, Δt=Δt)
-    # S = (Dict(2 => 0.1548,1 => 0.8452)
-    @test 0.79 < S[1] < 0.88
-    @test 0.12 < S[2] < 0.21
-    @test S[1] + S[2] == 1.0
+    # println("Test No. 1 Supervised, generator ics.")
+    fs = basin_fractions(ds, feature_extraction, ics_foo, attractors_ic; num_samples=N, T=Texec, Ttr=Ttr, Δt=Δt)
+    # fs = (Dict(2 => 0.8452,1 => 0.1548)
+    @test 0.12 < fs[1] < 0.21
+    @test 0.79 < fs[2] < 0.88
+
+
+    # println("Test No2. Unsupervised, generated ics.")
+    fs, class_labels = basin_fractions(ds, feature_extraction, ics; T=Texec, Ttr=Ttr, Δt=Δt)
+    # fs = (Dict(2 => 0.1548,1 => 0.8452)
+    @test 0.79 < fs[1] < 0.88
+    @test 0.12 < fs[2] < 0.21
+    @test fs[1] + fs[2] == 1.0
 
     #plot basins
     # using PyPlot
@@ -105,41 +103,39 @@ end
 
     s = sampler(min_bounds=min_limits, max_bounds=max_limits, method=sampling_method)
     ics_foo = s
-    Random.seed!(1)
     ics = [s() for i=1:N]
     ics = Dataset(ics)
 
 
     #---Running for supervised
     #templates
-    ic_templates = Dataset([0.21 0.02; 1.05 0.77; -0.67 0.02; -0.46 0.3; -0.43 0.12])
-    S, class_labels = basin_fractions(ds, feature_extraction, ics, ic_templates; T=Texec, Ttr=Ttr, Δt=Δt)
-    #original result: S = (Dict(4 => 0.0248,2 => 0.5086,3 => 0.028,5 => 0.2424,1 => 0.1962), [1, 2, 5, 2, 2, 1, 2, 1, 5, 5  …  2, 5, 1, 2, 2, 2, 2, 2, 1, 2])
+    attractors_ic = Dataset([0.21 0.02; 1.05 0.77; -0.67 0.02; -0.46 0.3; -0.43 0.12])
+    fs, class_labels = basin_fractions(ds, feature_extraction, ics, attractors_ic; T=Texec, Ttr=Ttr, Δt=Δt)
+    #original result: fs = (Dict(4 => 0.0248,2 => 0.5086,3 => 0.028,5 => 0.2424,1 => 0.1962), [1, 2, 5, 2, 2, 1, 2, 1, 5, 5  …  2, 5, 1, 2, 2, 2, 2, 2, 1, 2])
 
     #The results depend on the (random) sampling of the ics, so results may very on subsequent tests. I am not sure how much, though.
-    @test 0.18 < S[1] < 0.21
-    @test 0.48 < S[2] < 0.52
-    @test 0.02 < S[3] < 0.031
-    @test 0.01 < S[4] < 0.03
-    @test 0.22 < S[5] < 0.26
+    @test 0.18 < fs[1] < 0.21
+    @test 0.48 < fs[2] < 0.52
+    @test 0.02 < fs[3] < 0.031
+    @test 0.01 < fs[4] < 0.03
+    @test 0.22 < fs[5] < 0.26
 
-    Random.seed!(1)
-    S, class_labels = basin_fractions(ds, feature_extraction, ics_foo, ic_templates; num_samples=N, T=Texec, Ttr=Ttr, Δt=Δt)
-    @test 0.18 < S[1] < 0.21
-    @test 0.48 < S[2] < 0.52
-    @test 0.02 < S[3] < 0.031
-    @test 0.01 < S[4] < 0.03
-    @test 0.22 < S[5] < 0.26
+    fs = basin_fractions(ds, feature_extraction, ics_foo, attractors_ic; num_samples=N, T=Texec, Ttr=Ttr, Δt=Δt)
+    @test 0.18 < fs[1] < 0.21
+    @test 0.48 < fs[2] < 0.52
+    @test 0.02 < fs[3] < 0.031
+    @test 0.01 < fs[4] < 0.03
+    @test 0.22 < fs[5] < 0.26
 
 
-    S, class_labels = basin_fractions(ds, feature_extraction, ics; T=Texec, Ttr=Ttr, Δt=Δt)
-    #original result: S = (Dict(4 => 0.028,2 => 0.2424,3 => 0.1962,5 => 0.0248,1 => 0.5086), [3, 1, 2, 1, 1, 3, 1, 3, 2, 2  …  1, 2, 3, 1, 1, 1, 1, 1, 3, 1])
+    fs, class_labels = basin_fractions(ds, feature_extraction, ics; T=Texec, Ttr=Ttr, Δt=Δt)
+    #original result: fs = (Dict(4 => 0.028,2 => 0.2424,3 => 0.1962,5 => 0.0248,1 => 0.5086), [3, 1, 2, 1, 1, 3, 1, 3, 2, 2  …  1, 2, 3, 1, 1, 1, 1, 1, 3, 1])
 
-    @test 0.48 < S[1] < 0.52 
-    @test 0.22 < S[2] < 0.26 
-    @test 0.18 < S[3] < 0.21
-    @test 0.02 < S[4] < 0.031
-    @test 0.01 < S[5] < 0.03 
+    @test 0.48 < fs[1] < 0.52 
+    @test 0.22 < fs[2] < 0.26 
+    @test 0.18 < fs[3] < 0.21
+    @test 0.02 < fs[4] < 0.031
+    @test 0.01 < fs[5] < 0.03 
 
     #plot basins
     # cmap = matplotlib.colors.ListedColormap(["red", "gray", "orange", "cyan", "blue"])

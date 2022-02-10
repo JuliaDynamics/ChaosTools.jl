fc515c1546cb2e26e76a4f535799532458d42615
Datseris <datseris.george@gmail.com>
2022-02-10 22:30:13 +0100

    set the docstrings for new API

--- src/basins/basins_attractor_mapping.jl
+++ src/basins/basins_attractor_mapping.jl
@@ -1,27 +1,60 @@
 """
-    AttractorMapper(ds; kwargs...) → mapper
-Initialize a structure that maps initial conditions of `ds` to attractors.
-You can call `mapper` as a function of an initial condition:
+    AttractorMapper(ds, mapping_method; kwargs...) → mapper
+Initialize a structure that maps initial conditions of `ds` to attractors,
+using the given `mapping_method`.
+
+Currently available mapping methods:
+* [`AttractorsViaRecurrences`](@ref)
+* [`AttractorsViaProximity`](@ref)
+
+`AttractorMapper` can always be used directly with [`basin_fractions`](@ref).
+
+In addition, some mappers can be called as a function of an initial condition:
 ```julia
 label::Int = mapper(u0)
 ```
-and this will return the label of the attractor `u0` converges at.
-
-`AttractorMapper` identifies attractors via recurrences.
-It has the same keywords as [`basins_of_attraction`](@ref),
-so see there for how attractors are identified and what keywords you can use.
-**One of keywords `grid` or `attractors` is mandatory for `AttractorMapper`.**
-
-`AttractorMapper` has two modes of operation: **unsupervised**, with automatic detection of
-attractors and **supervised**, where the attractors are known.
-Both modes are in fact the same process that occurs in [`basins_of_attraction`](@ref),
-depending on whether the `attractors` keyword is given or not.
+and this will on the fly comput and return the label of the attractor `u0` converges at.
+The mappers that can do this are:
+* [`AttractorsViaRecurrences`](@ref)
+* [`AttractorsViaProximity`](@ref)
 """
 struct AttractorMapper{B<:BasinsInfo, I, K}
     bsn_nfo::B
     integ::I
     kwargs::K
 end
+
+abstract type AttractorMappingMethod end
+
+"""
+    AttractorsViaProximity(attractors::Dataset, ε = 1e-3)
+Map initial conditions to attractors based on whether the trajectory reaches `ε`-distance
+close to any of the user-provided `attractors`.
+
+The process works identically as "Refining basins of attraction" of 
+[`basins_of_attraction`](@ref). 
+
+Because in this method all possible attractors are already known to the user,
+the method can also be called **supervised**.
+"""
+struct AttractorsViaProximity <: AttractorMappingMethod
+    attractors::A
+    ε::Float64
+end
+
+"""
+    AttractorsViaRecurrences(args...)
+Map initial conditions to attractors by identifying attractors on the fly based on
+recurrences in the state space, as outlined by[^Datseris2022] and the 
+[`basins_of_attraction`](@ref) function.
+
+[^Datseris2022]: G. Datseris and A. Wagemakers, [Chaos 32, 023104 (2022)]( https://doi.org/10.1063/5.0076568)
+"""
+struct AttractorsViaRecurrences <: AttractorMappingMethod
+    field
+end
+
+
 
 function AttractorMapper(ds;
         # Notice that all of these are the same keywords as in `basins_of_attraction`
@@ -47,6 +80,8 @@ function AttractorMapper(ds;
     return AttractorMapper(bsn_nfo, integ, kwargs)
 end
 
+# TODO: Notice, currently this code assumes that all versions of `AttractorMapper`
+# use the low level code of `basins_of_attraction`.
 function (mapper::AttractorMapper)(u0; kwargs...)
     lab = get_label_ic!(mapper.bsn_nfo, mapper.integ, u0; mapper.kwargs...)
     return iseven(lab) ? (lab ÷ 2) : (lab - 1) ÷ 2

ae098451c8bef3b798478d9dc537594c9899ba68
Datseris <datseris.george@gmail.com>
2022-02-19 00:43:38 +0100

    improved fixed mass algorithm docstring

--- src/dimensions/correlationsum_fixedmass.jl
+++ src/dimensions/correlationsum_fixedmass.jl
@@ -4,25 +4,30 @@ using SpecialFunctions, Random
 
 """
     correlationsum_fixedmass(data, max_j; metric = Euclidean(), M = length(data)) → rs, ys
-A fixed mass algorithm for the calculation of the fractal dimension ``\\Delta`` according
-to [^Grassberger1988] with `max_j` the maximum number of neighbours that
-should be considered for the calculation, `M` defines the number of points
-considered for the calculation, default is the whole data set.
+A fixed mass algorithm for the calculation of a fractal dimension ``\\Delta`` 
+with `max_j` the maximum number of neighbours that
+should be considered for the calculation. `M` defines the number of points
+considered for the averaging of distances.
 
-Implements
+## Description
+The calculated ``\\Delta`` approximates the information dimension.
+The implementation here is due to to [^Grassberger1988], which defines
 ```math
-\\Delta \\overline{\\log r^{(j)}} \\sim Ψ(j) - \\log N
+\\Delta \\times \\overline{\\log r^{(j)}} \\sim Ψ(j) - \\log N
 ```
 where `` \\Psi(j) = \\frac{\\text{d} \\log Γ(j)}{\\text{d} j}
-``, `rs` = ``\\overline{\\log r^{(j)}}`` and `ys` = ``\\Psi(j) - \\log N``.
+``, `rs` = ``\\overline{\\log r^{(j)}}`` and `ys` = ``\\Psi(j) - \\log N``
+(``N`` is the length of the data).
 
 ``\\Delta`` can be computed by using `linear_region(rs, ys)`.
 
-[^Grassberger1988]: Peter Grassberger (1988) [Finite sample Corrections to Entropy and Dimension Estimates, Physics Letters A 128(6-7)](https://doi.org/10.1016/0375-9601(88)90193-4)
+[^Grassberger1988]: 
+    Peter Grassberger (1988) [Finite sample Corrections to Entropy and Dimension Estimates,
+    Physics Letters A 128(6-7)](https://doi.org/10.1016/0375-9601(88)90193-4)
 """
 function correlationsum_fixedmass(data, max_j; metric = Euclidean(), M = length(data), w = 0)
     N = length(data)
-    M > N && throw(ArgumentError("The number of points used for the calculation `M` should not exceed the number of points provided."))
+    @assert M ≤ N
     # Transform the data into a tree.
     tree = searchstructure(KDTree, data, metric)
     # Calculate all nearest neighbours up to max_j.

6a15fc735dbecb1b62cb3e0706dbaeb8c4393be0
Datseris <datseris.george@gmail.com>
2022-05-17 19:06:48 +0200

    also molteno docstring fix

--- src/dimensions/molteno.jl
+++ src/dimensions/molteno.jl
@@ -22,15 +22,17 @@ box into `2^D` new boxes if `D` is the dimension. For large `D` this leads to lo
 box divisions before the threshold is passed and the divison stops. This results
 to a low number of data points to fit the dimension to and thereby a poor estimate.
 
-[^Molteno1993]: Molteno, T. C. A., [Fast O(N) box-counting algorithm for estimating dimensions. Phys. Rev. E 48, R3263(R) (1993)](https://doi.org/10.1103/PhysRevE.48.R3263)
+[^Molteno1993]:
+    Molteno, T. C. A., [Fast O(N) box-counting algorithm for estimating dimensions.
+    Phys. Rev. E 48, R3263(R) (1993)](https://doi.org/10.1103/PhysRevE.48.R3263)
 """
 function molteno_dim(data; k0 = 10, α = nothing, q=1.0, base = ℯ)
     if α ≠ nothing
         @warn "Keyword `α` is deprecated in favor of `q`."
         q = α
     end
-    boxes, εs = molteno_boxing(data; k0)
-    dd = genentropy.(boxes; q, base)
+    probs, εs = molteno_boxing(data; k0)
+    dd = genentropy.(probs; q, base)
     return linear_region(-log.(base, εs), dd)[2]
 end
 
@@ -53,6 +55,11 @@ over the number of filled boxes falls below `k0`. The box sizes `εs` are
 calculated and returned together with the `probs`.
 
 See[^Molteno1993] for more.
+
+[^Molteno1993]:
+    Molteno, T. C. A., [Fast O(N) box-counting algorithm for estimating dimensions.
+    Phys. Rev. E 48, R3263(R) (1993)](https://doi.org/10.1103/PhysRevE.48.R3263)
+
 """
 function molteno_boxing(data::AbstractDataset; k0 = 10)
     integers, ε0 = real_to_uint64(data)

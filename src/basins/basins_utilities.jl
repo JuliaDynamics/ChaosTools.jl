107fe792f7159f9d63dbd760d4980b9a37f2b62c
Datseris <datseris.george@gmail.com>
2022-06-11 17:57:24 +0200

    finish overlap method and docs

--- src/basins/basins_utilities.jl
+++ src/basins/basins_utilities.jl
@@ -34,17 +34,27 @@ end
 # Thanks a lot to Valentin @Seelengrab for generous help in the key swapping code.
 """
     match_attractor_ids!(a₊::AbstractDict, a₋; metric = Euclidean(), threshold = Inf)
-Match attractor IDs in dictionary `a₊` so that its attractors that are the closest to
+Given dictionaries `a₊, a₋` mapping IDs to attractors,
+match attractor IDs in dictionary `a₊` so that its attractors that are the closest to
 those in dictionary `a₋` get assigned the same key as in `a₋`.
 Typically the +,- mean after and before some change of parameter of a system.
 Distance is quantified by the `metric`.
+
+Return the `replacement_map`, a dictionary mapping previous indices of `a₊` to
+the new ones that they were mapped to.
+
+When finding attractors and their fractions in DynamicalSystems.jl,
+different attractors get assigned different IDs. However
+which attractor gets which ID is somewhat arbitrary. Finding the attractors of the
+same system for slightly different parameters could label the "same" attractors (at
+the different parameters) with different IDs. `match_attractors!` tries to "match" them
+by modifying the attractor IDs.
 
 Optionally, you can provide a `threshold` value. If the distance between two attractors
 is larger than this `threshold`, then it is guaranteed that the attractors will get assigned
 different key in the dictionary `a₊`.
 
-Return the final `replacement_map`, a dictionary mapping previous indices of `a₊` to
-the new ones that they were mapped to.
+This function is always called at the end of [`basins_fractions_continuation`](@ref).
 
     match_attractor_ids!(as::AbstractVector{<:AbstractDict}; kwargs...)
 When given a vector of dictionaries, iteratively perform the above method for each
@@ -124,77 +134,38 @@ end
 # Matching with basins and possibly overlaps
 ###########################################################################################
 """
-    match_attractor_ids!(b₋, a₋, b₊, a₊, [, method = :distance]; metric = Euclidean())
-Match the attractors in basins/attractors `b₊, a₊` with those at `b₋, a₋`.
-`b` is an array whose values encode the attractor ID, while `a` is a dictionary mapping
-IDs to `Dataset`s containing the attractors (i.e, output of [`basins_of_attraction`](@ref)).
-Typically the +,- mean after and before some change of parameter for a system.
-
-In [`basins_of_attraction`](@ref) different attractors get assigned different IDs, however
-which attractor gets which ID is somewhat arbitrary, and computing the basins of the
-same system for slightly different parameters could label the "same" attractors (at
-the different parameters) with different IDs. `match_attractors!` tries to "match" them
-by modifying the attractor IDs.
-
+```julia
+match_attractor_ids!(b₊, a₊, b₋, a₋, [, method = :distance];
+                     metric = Euclidean(), threshold = Inf)
+```
+An enhancement of `match_attractor_ids!(a₊, a₋, ...)` that also takes in calculated
+basins of attraction `b::AbstractArray`. Once again match attractor IDs, now in both
+the dictionary `a₊` and the basins of attraction `b₊`. In addition, the
 `method` decides the matching process:
 * `method = :overlap` matches attractors whose basins of attraction before and after
-  have the most overlap (in pixels).
-* `method = :distance` matches attractors whose state space distance the smallest.
-  The keyword `metric` decides the metric for the distance (anything from Distances.jl).
-
-The modification of IDs is always done on the `b, a` that have *less* attractors.
-If they have equal, modification is done on the `₊` values.
-
-The modification of IDs is always done on the `₊` values. If they have more attractors
-than the `₋`, then a match is first done, and then the least matching attractors get
-any non used integers as keys.
+  have the most overlap (in pixels). This overlap is normalized in 0-1 (with 1 meaning
+  100% overlap of pixels). The `threshold` in this case is compared to the inverse
+  of the overlap.
+* `method = :distance` is the same as in `match_attractor_ids!(a₊, a₋, ...)`.
 """
-function match_attractor_ids!(b₋::AbstractArray, a₋::AbstractDict, b₊, a₊, method = :distance; metric = Euclidean())
-    error("TODO: Re-write in new code.")
-    # if length(a₊) > length(a₋)
-    #     # Set it up so that modification is always done on `+` attractors
-    #     a₋, a₊ = a₊, a₋
-    #     b₋, b₊ = b₊, b₋
-    # end
-    ids₊, ids₋ = keys(a₊), keys(a₋)
+function match_attractor_ids!(b₊::AbstractArray, a₊::AbstractDict, b₋, a₋,
+    method = :distance; metric = Euclidean(), threshold = Inf)
+
     if method == :overlap
-        distances = _similarity_from_overlaps(b₋, ids₋, b₊, ids₊)
+        ids₊, ids₋ = keys(a₊), keys(a₋)
+        distances = _similarity_from_overlaps(b₊, ids₊, b₋, ids₋)
     elseif method == :distance
-        distances = _similarity_from_distance(a₋, ids₋, a₊, ids₊, metric)
+        distances = _similarity_from_distance(a₊, a₋, metric)
     else
         error("Unknown `method` for `match_attractors!`.")
     end
-
-    replacement_map = _replacement_map(distances, ids₋, ids₊)
-
-    # Do the actual replacing; easy for the basin arrays
-    if b₊ isa AbstractArray
-        replace!(b₊, replacement_map...)
-    end
-    # But a bit more involved for the attractor dictionaries
-    aorig = copy(a₊)
-    for (k, v) ∈ replacement_map
-        a₊[v] = aorig[k]
-    end
-    # delete unused keys!
-    for k ∈ keys(a₊)
-        if k ∉ values(replacement_map); delete!(a₊, k); end
-    end
-    return similarity
-
-    # TODO: Modification of IDs must always be done on + version, because
-    # only this way I can maintain the replacement_mapping.
-
-    # For a replacement map, I can Do
-    # for (k, v) ∈ replacement_map
-    #     # a₊[v] = aorig[k]
-    #     a+[v] = pop!(a+, k)
-    # end
-
+    mdc = _minimal_distance_combinations(distances)
+    replacement_map = _swap_dict_keys!(a₊, a₋, mdc, threshold)
+    replace!(b₊, replacement_map...)
+    return replacement_map
 end
 
-
-function _similarity_from_overlaps(b₋, ids₋, b₊, ids₊)
+function _similarity_from_overlaps(b₊, ids₊, b₋, ids₋)
     @assert size(b₋) == size(b₊)
     distances = Dict{eltype(ids₊), Dict{eltype(ids₋), Float64}}[]
     for i in ids₊

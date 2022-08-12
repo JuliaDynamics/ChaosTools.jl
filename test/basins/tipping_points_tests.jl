4ab7b114938cd93b6e4b3145061706d35cdf1593
awage <>
2022-08-12 10:04:00 +0200

    Update basins tipping tests

--- test/basins/tipping_points_tests.jl
+++ test/basins/tipping_points_tests.jl
@@ -5,11 +5,12 @@ using Test
 @testset "Tipping points" begin
     xg = yg = range(-4, 4, length = 100)
     ds = Systems.magnetic_pendulum(d=0.2, α=0.2, ω=0.8, N=3)
-    basins_before, = basins_of_attraction((xg, yg), ds; Δt = 1., idxs=1:2,
-    diffeq=(;:reltol => 1e-9), show_progress = false)
-    ds = Systems.magnetic_pendulum(d=0.2, α=0.2, ω=0.8, N=3, γs = [1.0, 1.0, 0.05])
-    basins_after, = basins_of_attraction((xg, yg), ds; Δt = 1., idxs = 1:2,
-    diffeq=(;:reltol => 1e-9), show_progress = false)
+    pinteg = projected_integrator(ds, 1:2, [0,0];  diffeq=(;:reltol => 1e-9))
+    mapper = AttractorsViaRecurrences(pinteg, (xg, yg); Δt = 1.)     
+    basins_before, = basins_of_attraction(mapper; show_progress = false)
+    pinteg.integ.p.γs = [1., 1., 0.05] # Change magnet strength
+    mapper = AttractorsViaRecurrences(pinteg, (xg, yg); Δt = 1.)     
+    basins_after, = basins_of_attraction(mapper; show_progress = false)
     @testset "Basin fractions" begin
         fs = basins_fractions(basins_before)
         @test sum(values(fs)) ≈ 1

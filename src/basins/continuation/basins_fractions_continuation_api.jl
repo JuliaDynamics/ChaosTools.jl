d8a95825e86b563dfc39c60e8b7808b75052afd8
Datseris <datseris.george@gmail.com>
2022-09-20 20:31:56 +0100

    write return types of continuation API

--- src/basins/continuation/basins_fractions_continuation_api.jl
+++ src/basins/continuation/basins_fractions_continuation_api.jl
@@ -7,38 +7,21 @@ export basins_fractions_continuation
 abstract type BasinsFractionContinuation end
 
 """
-basins_fractions_continuation(continuation::BasinsFractionContinuation, parameter; kwargs...)
+    basins_fractions_continuation(continuation::BasinsFractionContinuation, parameter; kwargs...)
 Continiation API. TODO: Write it.
+
+Return:
+1. `fracs <: Vector{Dict{Int, Float64}}`. The fractions of basins of attraction.
+   `fracs[i]` is a dictionary mapping attractor IDs to their basin fraction
+   at the `i`-th parameter.
+2. `attractors_info <: Vector{Dict{Int, <:Any}}`. Information about the attractors.
+   `attractors_info[i]` is a dictionary mapping attractor ID to information about the
+   attractor. The type of information stored depends on the chosen continuation method.
 
 Current continiation methods are:
 - [`RecurrencesSeedingContinuation`](@ref).
 """
-function basins_fractions_continuation(continuation::BasinsFractionContinuation, parameter; kwargs...)
-    # code
-    # Return values:
-    # the fractions
-    fracs <: Vector{Dict{Int, Float64}}
-    # some info on the attractors which depends on the method.
-    # Could be the actual attractors (possible in recurrences),
-    # or the attractor features...?
-    attractor_summary <: Vector{Dict{Int, <:Any}}
-end
-
-function _example(; kwargs...)
-    # Decide the kind of dynamical system
-    α = 0.2; ω = 1.0; d = 0.3
-    ma = Systems.magnetic_pendulum(; α, ω, d)
-    proj = projected_integrator(ma, [1,2], [0,0])
-    # Decide the attractor mapping
-    gx = gy = range(-5, 5; length = 1500)
-    mapper = AttractorsViaRecurrences(proj, (gx, gy))
-    # Decide how to match attractors
-    continuation = RecurrencesSeedingContinuation(mapper, kwargs...)
-    # What parameter to continue over
-    parameter = (0:0.01:1, 1) # index, range
-    # Call function
-    basins_fractions_continuation(continuation, parameter; kwargs...)
-end
+function basins_fractions_continuation end
 
 include("match_attractor_ids.jl")
 include("continuation_recurrences.jl")
---
+++ src/basins/continuation/continuation_clustering.jl
@@ -0,0 +1,3 @@
+export RecurrencesSeedingContinuation
+import ProgressMeter
+

26bab6ce4fca74a55d9e9e3b4e2321a0593d96eb
awage <>
2022-10-18 10:26:03 +0200

    create function for clustering

--- src/basins/continuation/continuation_clustering.jl
+++ src/basins/continuation/continuation_clustering.jl
@@ -1,20 +1,18 @@
 export ClusteringAcrossParametersContinuation
 import ProgressMeter
 
-# TODO: Make this into a struct
 """
-
     ClusteringAcrossParametersContinuation(mapper::AttractorsViaFeaturizing; kwargs...)
-A method for [`basins_fractions_continuation`](@ref). This method computes the Features accross a range of parameters before performing a clustering into classes using a special metric, see [^Gelbrecht2020]. 
+A method to compute the continuation of a basin when a parameter changes, see [`basins_fractions_continuation`](@ref). This method computes the Features accross a range of parameters before performing a clustering into classes using a special metric, see [^Gelbrecht2020]. 
 
-The method takes as an input a `mapper` that maps initial conditions to attractors using the featurizing method [^Stender2021]. See [`AttractorMapper`](@ref) for how to use the `mapper`.
+The function takes as an input a `mapper` that maps initial conditions to attractors using the featurizing method [^Stender2021]. See [`AttractorMapper`](@ref) for how to use the `mapper`.
 
 ## Keyword Arguments
-- `prange` Range of parameters
-- `pidx` Number or symbol of the parameter to chabge in the array of parameters of the dynamical system. 
+- `prange` Range of parameter to analyze.
+- `pidx` Number or symbol of the parameter to change in the array of parameters of the dynamical system. 
 - `ics` Sampler function to generate initial conditions to sample. 
-- `samples_per_parameter` Number of samples per parameter
-- `par_weight`: This is the weight that ponderates the distance matrix according the parameter distance between two features. 
+- `samples_per_parameter` Number of samples per parameter.
+- `par_weight` The distance matrix between features has a special extra  weight that is proportional to the distance |p_i - p_j| between the parameters of each features. This keyword argument is the weight coeficient that ponderates the distance matrix.
 
 ## Description
 
@@ -81,20 +79,7 @@ function basins_fractions_continuation(
         end
     end
 
-    # Cluster them
-    f = reduce(hcat, features) # Convert to Matrix from Vector{Vector}
-    f = float.(f)
-    features_for_optimal = if cc.max_used_features == 0
-        f
-    else
-        StatsBase.sample(f, minimum(length(features), cc.max_used_features); replace = false)
-    end 
-    ϵ_optimal = optimal_radius_dbscan(
-        features_for_optimal, cc.min_neighbors, metric, cc.optimal_radius_method,
-        cc.num_attempts_radius, cc.silhouette_statistic
-    )
-    dbscanresult = dbscan(dists, ϵ_optimal, cc.min_neighbors)
-    cluster_labels = cluster_assignment(dbscanresult)
+    cluster_labels =  _cluster_features_across_parameters(features, dists)
 
     # And finally collect/group stuff into their dictionaries
     fractions_curves = Vector{Dict{Int, Float64}}(undef, n)
@@ -112,3 +97,23 @@ function basins_fractions_continuation(
     end
     return fractions_curves, attractors_info
 end
+
+
+function _cluster_features_across_parameters(features, dists)
+    # Cluster them
+    f = reduce(hcat, features) # Convert to Matrix from Vector{Vector}
+    f = float.(f)
+    features_for_optimal = if cc.max_used_features == 0
+        f
+    else
+        StatsBase.sample(f, minimum(length(features), cc.max_used_features); replace = false)
+    end 
+    ϵ_optimal = optimal_radius_dbscan(
+        features_for_optimal, cc.min_neighbors, metric, cc.optimal_radius_method,
+        cc.num_attempts_radius, cc.silhouette_statistic
+    )
+    dbscanresult = dbscan(dists, ϵ_optimal, cc.min_neighbors)
+    cluster_labels = cluster_assignment(dbscanresult)
+    return cluster_labels
+end
+

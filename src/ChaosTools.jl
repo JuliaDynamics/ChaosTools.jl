070133f37b46bb8cee7bca8b3d5e437cbbf4ac7c
Datseris <datseris.george@gmail.com>
2023-02-07 09:58:39 +0000

    remove all files form include until update

--- src/ChaosTools.jl
+++ src/ChaosTools.jl
@@ -7,51 +7,28 @@ using DelayEmbeddings
 using ComplexityMeasures
 using DynamicalSystemsBase
 
-using DynamicalSystemsBase: DS, DDS, CDS
-using DynamicalSystemsBase: MDI, TDI
-using DynamicalSystemsBase: stateeltype
-using DynamicalSystemsBase.SciMLBase: AbstractODEIntegrator, u_modified!, DEIntegrator
-using Optim
-DEI = DEIntegrator
 
-include("orbitdiagrams/discrete_diagram.jl")
-include("orbitdiagrams/produce_orbitdiagram.jl")
+# include("orbitdiagrams/discrete_diagram.jl")
+# include("orbitdiagrams/produce_orbitdiagram.jl")
 
-include("fractaldim/linear_regions.jl")
-include("fractaldim/generalized_dim.jl")
-include("fractaldim/correlationsum_vanilla.jl")
-include("fractaldim/correlationsum_boxassisted.jl")
-include("fractaldim/correlationsum_fixedmass.jl")
-include("fractaldim/molteno.jl")
-include("fractaldim/kaplanyorke.jl")
-include("fractaldim/takens_best_estimate.jl")
-include("fractaldim/higuchi.jl")
+# include("dimreduction/broomhead_king.jl")
+# include("dimreduction/dyca.jl")
 
-include("dimreduction/broomhead_king.jl")
-include("dimreduction/dyca.jl")
+# include("stablemotion/periodic_points.jl")
+# include("stablemotion/period.jl")
+# include("stablemotion/fixedpoints.jl")
+# include("stablemotion/yin.jl")
 
-include("stablemotion/periodic_points.jl")
-include("stablemotion/period.jl")
-include("stablemotion/fixedpoints.jl")
-include("stablemotion/yin.jl")
+# include("rareevents/mean_return_times/mrt_api.jl")
 
-include("rareevents/mean_return_times/mrt_api.jl")
-
-include("chaosdetection/lyapunovs/lyapunovspectrum.jl")
+# include("chaosdetection/lyapunovs/lyapunovspectrum.jl")
 include("chaosdetection/lyapunovs/lyapunov.jl")
-include("chaosdetection/lyapunovs/local_growth_rates.jl")
-include("chaosdetection/lyapunovs/lyapunov_from_data.jl")
-include("chaosdetection/gali.jl")
-include("chaosdetection/expansionentropy.jl")
-include("chaosdetection/partially_predictable.jl")
-include("chaosdetection/testchaos01.jl")
-
-# Exports
-# TODO: It is probably better to put all exports here rather than in
-# each individual file. Or is it...? Not sure yet.
-export lyapunovspectrum, lyapunov, local_growth_rates, lyapuov_from_data
-
-
+# include("chaosdetection/lyapunovs/local_growth_rates.jl")
+# include("chaosdetection/lyapunovs/lyapunov_from_data.jl")
+# include("chaosdetection/gali.jl")
+# include("chaosdetection/expansionentropy.jl")
+# include("chaosdetection/partially_predictable.jl")
+# include("chaosdetection/testchaos01.jl")
 
 include("deprecations.jl")
 

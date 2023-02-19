aa5285ec9719982201c9d6f7a51a55f96d44be56
Datseris <datseris.george@gmail.com>
2023-02-19 12:46:37 +0000

    use simpler docbuild

--- docs/make.jl
+++ docs/make.jl
@@ -1,15 +1,7 @@
 cd(@__DIR__)
+using ChaosTools, DynamicalSystemsBase, Neighborhood
 
-import Downloads
-Downloads.download(
-    "https://raw.githubusercontent.com/JuliaDynamics/doctheme/master/apply_style.jl",
-    joinpath(@__DIR__, "apply_style.jl")
-)
-include("apply_style.jl")
-
-using ChaosTools, Neighborhood
-
-CHAOSTOOLS_PAGES = [
+pages = [
     "index.md",
     "orbitdiagram.md",
     "lyapunovs.md",
@@ -19,26 +11,11 @@ CHAOSTOOLS_PAGES = [
     "rareevents.md",
 ]
 
-makedocs(
-    modules = [ChaosTools, DynamicalSystemsBase, Neighborhood],
-    format = Documenter.HTML(
-        prettyurls = CI,
-        assets = [
-            asset("https://fonts.googleapis.com/css?family=Montserrat|Source+Code+Pro&display=swap", class=:css),
-        ],
-        collapselevel = 3,
-    ),
-    sitename = "ChaosTools.jl",
-    authors = "George Datseris",
-    pages = CHAOSTOOLS_PAGES,
-    doctest = false,
-    draft = false,
+import Downloads
+Downloads.download(
+    "https://raw.githubusercontent.com/JuliaDynamics/doctheme/master/build_docs_with_style.jl",
+    joinpath(@__DIR__, "build_docs_with_style.jl")
 )
+include("build_docs_with_style.jl")
 
-if CI
-    deploydocs(
-        repo = "github.com/JuliaDynamics/ChaosTools.jl.git",
-        target = "build",
-        push_preview = true
-    )
-end
+build_docs_with_style(pages, ChaosTools, DynamicalSystemsBase, Neighborhood)
